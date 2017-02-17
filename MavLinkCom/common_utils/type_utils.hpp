aee16a5028cd085d037bdc465a645c0cabef01e0
Patrick José Pereira <patrickelectric@gmail.com>
2017-02-17 16:57:25 -0200

    type_utils.hpp: Solve typos
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- MavLinkCom/common_utils/type_utils.hpp
+++ MavLinkCom/common_utils/type_utils.hpp
@@ -30,7 +30,7 @@ namespace common_utils { namespace type_utils {
             static const bool value = sizeof(test<T>(nullptr)) == sizeof(yes);
             using type =  T;
 
-            void dummy(); //for GCC to supress -Wctor-dtor-privacy
+            void dummy(); //for GCC to suppress -Wctor-dtor-privacy
         };
 
         template <typename T>
@@ -55,7 +55,7 @@ namespace common_utils { namespace type_utils {
             static bool const beg_value = sizeof(f<T>(nullptr)) == sizeof(yes);
             static bool const end_value = sizeof(g<T>(nullptr)) == sizeof(yes);
 
-            void dummy(); //for GCC to supress -Wctor-dtor-privacy
+            void dummy(); //for GCC to suppress -Wctor-dtor-privacy
         };
 
     }  // namespace detail
