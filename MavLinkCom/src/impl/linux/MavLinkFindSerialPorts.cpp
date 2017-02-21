67ee7b884c02ef8045b2b6611820051c1cf1a5aa
clovett <clovett@microsoft.com>
2017-02-21 11:52:39 -0800

    Fix mismatching delete[]

--- MavLinkCom/src/impl/linux/MavLinkFindSerialPorts.cpp
+++ MavLinkCom/src/impl/linux/MavLinkFindSerialPorts.cpp
@@ -8,7 +8,7 @@ using namespace mavlinkcom_impl;
 
 std::vector<SerialPortInfo> MavLinkConnectionImpl::findSerialPorts(int vid, int pid)
 {
-    // not implemented on Linux yet...  probably need to do an lstat on '/dev/serial/by-id' and find
+    // todo: not implemented on Linux yet...  probably need to do an lstat on '/dev/serial/by-id' and find
     // something that looks like PX4 and return that name, or follow the symbolic link to /dev/ttyACM0...
     std::vector<SerialPortInfo> result;
     return result;
