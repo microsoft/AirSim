6038da61f3a28773425659a8259b52c309b4a007
Chris Lovett <clovett@microsoft.com>
2017-02-20 16:19:50 -0800

    Minor fixes for Linux build.

--- cmake/cmake-modules/FindEigen3.cmake
+++ cmake/cmake-modules/FindEigen3.cmake
@@ -63,8 +63,8 @@ else (EIGEN3_INCLUDE_DIR)
 
   find_path(EIGEN3_INCLUDE_DIR NAMES signature_of_eigen3_matrix_library
       PATHS
-      ${CMAKE_INSTALL_PREFIX}/include
       $ENV{EIGEN_ROOT}
+      ${CMAKE_INSTALL_PREFIX}/include
       ${KDE4_INCLUDE_DIR}
       PATH_SUFFIXES eigen3 eigen
     )
