777d97086bb03310fc8bacffbb479cf6ebe9e4a4
Chris Lovett <clovett@microsoft.com>
2017-04-07 14:36:47 -0700

    add unit tests.

---
+++ AirLibUnitTests/AirLibUnitTests.cpp
@@ -0,0 +1,12 @@
+// AirLibUnitTests.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <cassert>
+
+int main()
+{
+	assert(1 + 1 > 1);
+    return 0;
+}
+
