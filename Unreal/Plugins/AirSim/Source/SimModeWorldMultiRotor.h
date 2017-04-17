63e80c43812300a8570b04ed42714a3f6949e63f
saihv <saihemachandra.v@gmail.com>
2017-04-16 21:14:13 -0500

    Separate thread for recording

--- Unreal/Plugins/AirSim/Source/SimModeWorldMultiRotor.h
+++ Unreal/Plugins/AirSim/Source/SimModeWorldMultiRotor.h
@@ -15,14 +15,14 @@ public:
     virtual void BeginPlay() override;
     virtual void Tick( float DeltaSeconds ) override;
     virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
+	std::shared_ptr<VehicleConnectorBase> fpv_vehicle_connector_;
 
 protected:
     virtual void createVehicles(std::vector<VehiclePtr>& vehicles) override;
     bool checkConnection();
     VehiclePtr createVehicle(AFlyingPawn* pawn);
 
-private:
-    std::shared_ptr<VehicleConnectorBase> fpv_vehicle_connector_;
+private:    
     TArray<uint8> image_;
-
+	bool isLoggingStarted;
 };
