5e6faab7399aea90daff2e527537f8577bf97b57
Chris Lovett <clovett@microsoft.com>
2017-06-13 16:49:31 -0700

    fix gcc build script

--- cmake/build.sh
+++ cmake/build.sh
@@ -1,4 +1,10 @@
-# Get the location of this script, it is assumed you already cloned "main" and you are running the
+# This script is for building AirSim with GCC-6
+# You should NOT use this with Unreal.  
+# If you want bits that work in Unreal you need to use ../build.sh
+# For pre-requisite tools needed to run this script see
+# https://github.com/Microsoft/AirSim/blob/master/docs/linux_build.md
+
+# First get the location of this script, it is assumed you already cloned "main" and you are running the
 # script from that repo.  It will then put everything else in a ROS workspace at "../catkin_ws"
 SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
@@ -7,22 +13,33 @@ pushd $SCRIPTPATH
 # update the rpclib git submodule
 pushd ..
 git submodule update --init --recursive 
+
+if [[ ! -d "$EIGEN_ROOT" ]]; then
+        if [[ ! -d eigen ]]; then
+                echo "downloading eigen..."
+                wget http://bitbucket.org/eigen/eigen/get/3.3.2.zip
+                unzip 3.3.2.zip -d eigen
+                pushd eigen
+                mv eigen* eigen3
+                echo "3.3.2" > version
+                popd &>/dev/null
+                rm 3.3.2.zip
+        fi
+        export EIGEN_ROOT="$(pwd)/eigen"
+fi
 popd
 
 if [ -f "CMakeCache.txt" ]; then
 rm CMakeCache.txt 
 fi
 
-if [ -f "CMakeFiles" ]; then
+if [ -d "CMakeFiles" ]; then
 rm -rf CMakeFiles
 fi
 
 GCCARGS="-D CMAKE_BUILD_TYPE=Debug"
 
-GCCVERSION=$(gcc -v 2>&1 | sed -n "/^gcc version/p" | sed -e "s/^gcc version \([0-9]\)/\1/g" | cut -d"." -f1)
-if [ $GCCVERSION -lt 5 ]; then
-  GCCARGS="$GCCARGS -D CMAKE_C_COMPILER=gcc-6 -D CMAKE_CXX_COMPILER=g++-6"
-fi
+GCCARGS="$GCCARGS -D CMAKE_C_COMPILER=gcc-6 -D CMAKE_CXX_COMPILER=g++-6"
 
 cmake $GCCARGS CMakeLists.txt
 
