467d04f59c73182ca672581fcc1d172773cce5c3
Shital Shah <shital@ShitalShah.com>
2017-07-28 23:11:58 -0700

    better tuning for simple_flight

--- AirLib/include/controllers/simple_flight/firmware/Params.hpp
+++ AirLib/include/controllers/simple_flight/firmware/Params.hpp
@@ -24,12 +24,22 @@ public:
     float min_armed_output = 0.2f;
 
     static constexpr float pi = 3.14159265359f; //180-degrees
-    float max_pitch_rate = 2.0f, max_roll_rate = 2.0f, max_yaw_rate = 2.0f; //in radians/sec
-    float max_pitch_angle = pi / 6, max_roll_angle = pi / 6, max_yaw_angle = pi; // in radians
+
+    //max_xxx_rate > 5 would introduce wobble/oscillations
+    static constexpr float kMaxRate = 2.5f;
+    float max_pitch_rate = kMaxRate, max_roll_rate = kMaxRate, max_yaw_rate = kMaxRate; //in radians/sec
+
+    //max_pitch/roll_angle > 5.5 would produce verticle thrust that is not enough to keep vehicle in air at extremeities of controls
+    float max_pitch_angle = pi / 5.5, max_roll_angle = pi / 5.5, max_yaw_angle = pi; // in radians
 
     //stabilizer params
-    float p_pitch_rate = 0.5f, p_roll_rate = 0.5f, p_yaw_rate = 0.5f;
-    float p_pitch_angle = 1.5f, p_roll_angle = 1.5f, p_yaw_angle = 1.5f;
+    //p_xxx_rate params are sensetive to gyro noise. Values higher than 0.5 would require 
+    //noise filteration
+    static constexpr float kPRateDefault = 0.5f;
+    float p_pitch_rate = kPRateDefault, p_roll_rate = kPRateDefault, p_yaw_rate = kPRateDefault;
+    
+    static constexpr float kPAngleDefault = 2.5f;
+    float p_pitch_angle = kPAngleDefault, p_roll_angle = kPAngleDefault, p_yaw_angle = kPAngleDefault;
 
     ControlMode default_control_mode = ControlMode::Angle;
 };
