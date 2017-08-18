95f35cdc9a0baff6bdc0b89625a1efcf07826dd3
Shital Shah <shital@ShitalShah.com>
2017-08-17 21:21:25 -0700

    depth in world units, added normals

--- Unreal/Plugins/AirSim/Source/PIPCamera.cpp
+++ Unreal/Plugins/AirSim/Source/PIPCamera.cpp
@@ -16,6 +16,8 @@ void APIPCamera::PostInitializeComponents()
     screen_capture_ = UAirBlueprintLib::GetActorComponent<USceneCaptureComponent2D>(this, TEXT("SceneCaptureComponent"));
     depth_capture_ = UAirBlueprintLib::GetActorComponent<USceneCaptureComponent2D>(this, TEXT("DepthCaptureComponent"));
     seg_capture_ = UAirBlueprintLib::GetActorComponent<USceneCaptureComponent2D>(this, TEXT("SegmentationCaptureComponent"));
+    normals_capture_ = UAirBlueprintLib::GetActorComponent<USceneCaptureComponent2D>(this, TEXT("NormalsCaptureComponent"));
+
 
     //set default for brigher images
     scene_capture_settings_.target_gamma = CaptureSettings::kSceneTargetGamma;
@@ -28,16 +30,20 @@ void APIPCamera::BeginPlay()
     screen_capture_->CaptureSource = ESceneCaptureSource::SCS_FinalColorLDR;
     depth_capture_->CaptureSource = ESceneCaptureSource::SCS_FinalColorLDR;
     seg_capture_->CaptureSource = ESceneCaptureSource::SCS_FinalColorLDR;
-    
+    normals_capture_->CaptureSource = ESceneCaptureSource::SCS_FinalColorLDR;
+        
     scene_render_target_ = NewObject<UTextureRenderTarget2D>();
     setCaptureSettings(ImageType_::Scene, scene_capture_settings_);
     //scene_render_target_->bHDR = false;
 
+    seg_render_target_ = NewObject<UTextureRenderTarget2D>();
+    setCaptureSettings(ImageType_::Segmentation, seg_capture_settings_);
+
     depth_render_target_ = NewObject<UTextureRenderTarget2D>();
     setCaptureSettings(ImageType_::Depth, depth_capture_settings_);
 
-    seg_render_target_ = NewObject<UTextureRenderTarget2D>();
-    setCaptureSettings(ImageType_::Segmentation, seg_capture_settings_);
+    normals_render_target_ = NewObject<UTextureRenderTarget2D>();
+    setCaptureSettings(ImageType_::Normals, normals_capture_settings_);
 }
 
 void APIPCamera::EndPlay(const EEndPlayReason::Type EndPlayReason)
@@ -78,6 +84,7 @@ void APIPCamera::setEnableCameraTypes(APIPCamera::ImageType types)
     enableCaptureComponent(ImageType_::Depth, (types & ImageType_::Depth));
     enableCaptureComponent(ImageType_::Scene, (types & ImageType_::Scene));
     enableCaptureComponent(ImageType_::Segmentation, (types & ImageType_::Segmentation));
+    enableCaptureComponent(ImageType_::Normals, (types & ImageType_::Normals));
 }
 
 APIPCamera::CaptureSettings APIPCamera::getCaptureSettings(ImageType_ type)
@@ -85,8 +92,9 @@ APIPCamera::CaptureSettings APIPCamera::getCaptureSettings(ImageType_ type)
     switch (type)
     {
     case ImageType_::Scene: return scene_capture_settings_;
-    case ImageType_::Depth: return depth_capture_settings_;
     case ImageType_::Segmentation: return seg_capture_settings_;
+    case ImageType_::Depth: return depth_capture_settings_;
+    case ImageType_::Normals: return normals_capture_settings_;
     default:
         throw std::invalid_argument("the ImageType specified for getCaptureSettings is not recognized");
     }
@@ -101,14 +109,19 @@ void APIPCamera::setCaptureSettings(APIPCamera::ImageType_ type, const APIPCamer
         updateCaptureComponentSettings(screen_capture_, scene_render_target_, scene_capture_settings_);
         break;
     }
+    case ImageType_::Segmentation: {
+        seg_capture_settings_ = settings;
+        updateCaptureComponentSettings(seg_capture_, seg_render_target_, seg_capture_settings_);
+        break;
+    }
     case ImageType_::Depth: {
         depth_capture_settings_ = settings;
         updateCaptureComponentSettings(depth_capture_, depth_render_target_, depth_capture_settings_);
         break;
     }
-    case ImageType_::Segmentation: {
-        seg_capture_settings_ = settings;
-        updateCaptureComponentSettings(seg_capture_, seg_render_target_, seg_capture_settings_);
+    case ImageType_::Normals: {
+        normals_capture_settings_ = settings;
+        updateCaptureComponentSettings(normals_capture_, normals_render_target_, normals_capture_settings_);
         break;
     }
     default:
@@ -182,13 +195,17 @@ UTextureRenderTarget2D* APIPCamera::getRenderTarget(const APIPCamera::ImageType 
         if (!if_active || (enabled_camera_types_ & ImageType_::Scene))
             return scene_render_target_;
         return nullptr;
+    case ImageType_::Segmentation:
+        if (!if_active || (enabled_camera_types_ & ImageType_::Segmentation))
+            return seg_render_target_;
+        return nullptr;
     case ImageType_::Depth:
         if (!if_active || (enabled_camera_types_ & ImageType_::Depth))
             return depth_render_target_;
         return nullptr;
-    case ImageType_::Segmentation:
-        if (!if_active || (enabled_camera_types_ & ImageType_::Segmentation))
-            return seg_render_target_;
+    case ImageType_::Normals:
+        if (!if_active || (enabled_camera_types_ & ImageType_::Normals))
+            return normals_render_target_;
         return nullptr;
     case ImageType_::None:
         return nullptr;
@@ -206,13 +223,17 @@ USceneCaptureComponent2D* APIPCamera::getCaptureComponent(const APIPCamera::Imag
         if (!if_active || (enabled_camera_types_ & ImageType_::Scene))
             return screen_capture_;
         return nullptr;
+    case ImageType_::Segmentation:
+        if (!if_active || (enabled_camera_types_ & ImageType_::Segmentation))
+            return seg_capture_;
+        return nullptr;
     case ImageType_::Depth:
         if (!if_active || (enabled_camera_types_ & ImageType_::Depth))
             return depth_capture_;
         return nullptr;
-    case ImageType_::Segmentation:
-        if (!if_active || (enabled_camera_types_ & ImageType_::Segmentation))
-            return seg_capture_;
+    case ImageType_::Normals:
+        if (!if_active || (enabled_camera_types_ & ImageType_::Normals))
+            return normals_capture_;
         return nullptr;
     case ImageType_::None:
         return nullptr;
@@ -229,6 +250,7 @@ void APIPCamera::disableAllPIP()
     enableCaptureComponent(ImageType_::Scene, false);
     enableCaptureComponent(ImageType_::Depth, false);
     enableCaptureComponent(ImageType_::Segmentation, false);
+    enableCaptureComponent(ImageType_::Normals, false);
 }
 
 
