b29c2aa92c179f6a74b6daccd33758973c4ef864
Shital Shah <sytelus@gmail.com>
2017-12-07 01:58:07 -0800

    Remove unnecessary line

--- PythonClient/objects.py
+++ PythonClient/objects.py
@@ -5,8 +5,6 @@ import pprint
 client = MultirotorClient()
 client.confirmConnection()
 
-p = Pose(Vector3r(), Quaternionr())
-
 pose = client.simGetObjectPose("TemplateCube_Rounded_17");
 print("Position: %s, Orientation: %s" % (pprint.pformat(pose.position),
     pprint.pformat(pose.orientation)))
