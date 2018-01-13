1ad9f75006f37ce65e4809d11921233790c6df69
Leland Johnson <Leland.Johnson@TechFam.com>
2018-01-13 16:18:17 -0700

    CMD file redirect not working on some system. Changes to have PowerShell perform the redirect to file.

--- Unreal/Environments/Blocks/GenerateProjectFiles.bat
+++ Unreal/Environments/Blocks/GenerateProjectFiles.bat
@@ -1,6 +1,6 @@
 setlocal
 del /q gen_temp.txt
-powershell -command "& { (Get-ItemProperty 'Registry::HKEY_CLASSES_ROOT\Unreal.ProjectFile\shell\rungenproj' -Name 'Icon' ).'Icon' }" > gen_temp.txt
+powershell -command "& { (Get-ItemProperty 'Registry::HKEY_CLASSES_ROOT\Unreal.ProjectFile\shell\rungenproj' -Name 'Icon' ).'Icon' } > gen_temp.txt"
 set /p gen_bin=<gen_temp.txt
 del /q gen_temp.txt
 for %%f in (*.uproject) do (
