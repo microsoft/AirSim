ac8064a6a54a6bb73d4567be27a3f92ad34cabf2
Matthias Muller <t-mamull@microsoft.com>
2018-08-01 14:17:47 -0700

    Added DepthNav subclass to compute next pose based on cost.

--- Examples/main.cpp
+++ Examples/main.cpp
@@ -80,15 +80,17 @@ void runGaussianMarkovTest()
 void runDepthNavTest()
 {
     Pose startPose = Pose(Vector3r(0, 0, -1), Quaternionr(1, 0, 0, 0)); //start pose
-    Pose goalPose = Pose(Vector3r(50, 20, -1), Quaternionr(1, 0, 0, 0)); //final pose
-
+    //Pose goalPose = Pose(Vector3r(50, 20, -1), Quaternionr(1, 0, 0, 0)); //final pose
+    Pose goalPose = Pose(Vector3r(50, 105, -1), Quaternionr(1, 0, 0, 0)); //final pose
 
     RpcLibClientBase client;
     client.confirmConnection();
 
     client.simSetVehiclePose(startPose, true);
+    std::cout << "Press Enter to start" << std::endl; std::cin.get(); //Allow some time to reach startPose
 
-    DepthNavThreshold depthNav;
+    //DepthNavThreshold depthNav;
+    DepthNavCost depthNav;
     depthNav.gotoGoal(goalPose, client);
 }
 
