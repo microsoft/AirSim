4549d63cee09988ee14f77ca58a5f390f0b2663b
Shital Shah <shital@ShitalShah.com>
2018-10-08 16:11:12 -0700

    Removed Saved folder deletion because UE build tool isn't happy

--- Unreal/Environments/Blocks/clean.bat
+++ Unreal/Environments/Blocks/clean.bat
@@ -1,7 +1,7 @@
 rd /s/q Build
 rd /s/q Binaries
 rd /s/q Intermediate
-rd /s/q Saved
+rem rd /s/q Saved
 rd /s/q Plugins\AirSim\Binaries
 rd /s/q Plugins\AirSim\Intermediate
 rd /s/q Plugins\AirSim\Saved
