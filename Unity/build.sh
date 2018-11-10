1752c537597fe3adfb4c4bf1005f7ef536048583
msb336 <msb336@drexel.edu>
2018-11-10 09:16:38 -0800

    Add cross-platform support for Unity
    bug fix (#1579) and SceneSelector bug fix
    
    added caveat for loading scenes.

---
+++ Unity/build.sh
@@ -0,0 +1,24 @@
+if [ ! -f ../external/rpclib/rpclib-2.2.1/rpclib.pc.in ]; then
+    >&2 echo "error, rpc.pc.in not found"
+fi
+cp ../external/rpclib/rpclib-2.2.1/rpclib.pc.in AirLibWrapper/AirsimWrapper/rpclib.pc.in
+
+if [ ! -d "linux-build" ]; then
+    mkdir linux-build;
+fi
+
+cd linux-build;
+export CC="clang-5.0"
+export CXX="clang++-5.0"
+
+cmake ../AirLibWrapper/AirsimWrapper;
+make -j`nproc`;
+if [ ! -d "../UnityDemo/Assets/Plugins" ]; then
+	mkdir ../ UnityDemo/Assets/Plugins;
+fi
+cp libAirsimWrapper.so ../UnityDemo/Assets/Plugins;
+
+cd ..
+if [ -f AirLibWrapper/AirsimWrapper/rpclib.pc.in ]; then
+	rm AirLibWrapper/AirsimWrapper/rpclib.pc.in
+fi
