d9d98ffd3b7b68c415ade34a1ea62d40c69733f9
madratman <ratneshmadaan@gmail.com>
2019-05-25 21:41:44 -0700

    wip

--- ros/src/trajectory_planner/include/traj_gen_helper.h
+++ ros/src/trajectory_planner/include/traj_gen_helper.h
@@ -7,6 +7,11 @@
 #include <mav_msgs/eigen_mav_msgs.h>
 #include "airsim_ethz_asl_adaptor.h"
 
+// todo make a generic interface to test and benchmark spline fitters / smoothers from various labs 
+// current version is adapted from ethz-asl's code
+// ref: https://github.com/ethz-asl/mav_trajectory_generation#linear-optimization
+// ref: https://github.com/ethz-asl/mav_voxblox_planning's PolynomialSmoother::getTrajectoryBetweenWaypoints()
+// ref: https://bitbucket.org/castacks/mav_fcs/src/master/
 class TrajGenHelper
 {
 public:
@@ -15,9 +20,38 @@ public:
 	bool waypoint_cb(const airsim_ros_pkgs::PointArray& waypoints_msg);
 
 private:
-	airsim_ros::DynamicConstraintsVelAccYaw dynamic_constraints_;
+	void traj_pub_timer_cb(const ros::TimerEvent& event);
+
 	ros::NodeHandle nh_;
 	ros::NodeHandle nh_private_;
+
+	ros::CallbackQueue command_publishing_queue_;
+	ros::AsyncSpinner command_publishing_spinner_;
+	ros::CallbackQueue planning_queue_;
+	ros::AsyncSpinner planning_spinner_;
+
+	ros::Subscriber waypoint_sub_;
+	ros::Subscriber waypoint_list_sub_;
+	ros::Publisher command_pub_;
+	ros::Publisher path_marker_pub_;
+	ros::Publisher full_trajectory_pub_;
+
+	mav_msgs::EigenTrajectoryPointVector waypoints_;
+	int current_waypoint_idx_;
+
+	mav_msgs::EigenTrajectoryPointVector path_queue_;
+	size_t path_idx_;
+	std::recursive_mutex path_mutex_;
+
+	int max_failures_;
+	int num_failures_;
+
+    nav_msgs::Odometry curr_odom_;
+
+	double replan_dt_;
+	double replan_lookahead_sec_;
+
+	airsim_ros::DynamicConstraintsVelAccYaw dynamic_constraints_;
 	int derivative_to_optimize_;
 	int order_poly_;
 	int magic_fabian_constant_;
--- ros/src/trajectory_planner/src/traj_gen_helper.cpp
+++ ros/src/trajectory_planner/src/traj_gen_helper.cpp
@@ -2,77 +2,146 @@
 
 TrajGenHelper::TrajGenHelper(const ros::NodeHandle &nh, const ros::NodeHandle &nh_private)
 {
-	nh_ = nh;
-	nh_private_ = nh_private;
-	nh_private_.getParam("derivative_to_optimize", derivative_to_optimize_);
-	if (derivative_to_optimize_ == 3) // snap
-		order_poly_ = 10;
+    nh_ = nh;
+    nh_private_ = nh_private;
+    nh_private_.getParam("derivative_to_optimize", derivative_to_optimize_);
+    if (derivative_to_optimize_ == 3) // snap
+        order_poly_ = 10;
 
-	if (derivative_to_optimize_ == 2) // jerk
-		order_poly_ = 8;
+    if (derivative_to_optimize_ == 2) // jerk
+        order_poly_ = 8;
 
-	dynamic_constraints_.load_from_rosparams(nh_);
-	magic_fabian_constant_ = 6.5; // in hardcoded magic, we trust
+    dynamic_constraints_.load_from_rosparams(nh_);
+    magic_fabian_constant_ = 6.5; // in hardcoded magic, we trust
+
+
+    // todo auto init odom topic. scale to multiple drones in airsim
+    odometry_sub_ = nh_.subscribe("/airsim_node/drone_1/odom_ned", 1, &TrajGenHelper::odom_cb, this);
+    waypoint_list_sub_ = nh_.subscribe("waypoint_list", 1, &TrajGenHelper::waypt_vec_cb, this);
+    command_pub_ = nh_.advertise<trajectory_msgs::MultiDOFJointTrajectory>(mav_msgs::default_topics::COMMAND_TRAJECTORY, 1);
 }
 
-// ref: https://github.com/ethz-asl/mav_trajectory_generation#linear-optimization
-// mav_voxblox_planning's PolynomialSmoother::getTrajectoryBetweenWaypoints()
-bool TrajGenHelper::waypoint_cb(const airsim_ros_pkgs::PointArray& waypoints_msg)
+void TrajGenHelper::odom_cb(const nav_msgs::Odometry& msg)
 {
-	const mav_msgs::EigenTrajectoryPoint::Vector waypoints = airsim_ethz_asl_adaptor::eigenTrajectoryPointVecFromAirsimMsg(waypoints_msg);
-	const int dimension = 3; // todo param
+    has_odom_ = true;
+    curr_odom_ = odom_msg;
+}
 
-	mav_trajectory_generation::Vertex::Vector vertices;
-	int counter = 0;
-	for (auto& curr_waypt : waypoints)
-	{
-		mav_trajectory_generation::Vertex vertex(dimension);
+void TrajGenHelper::clear_traj()
+{
+    std::lock_guard<std::recursive_mutex> guard(path_mutex_);
+    command_publishing_timer_.stop();
+    path_queue_.clear();
+    path_index_ = 0;
+}
 
-		// if first or last waypoint
-		if ((counter == 0) or (counter == waypoints.size()-1))
-		{
-			vertex.makeStartOrEnd(0, derivative_to_optimize_);
-			vertex.addConstraint(mav_trajectory_generation::derivative_order::POSITION, curr_waypt.position_W);
-		}
-		// if in the middle, add a position constraint for solver
-		// todo support higher order constraints
-		else
-		{
-			vertex.addConstraint(mav_trajectory_generation::derivative_order::POSITION, curr_waypt.position_W);  
-		}
-		vertices.push_back(vertex);
-	}
+void MavLocalPlanner::planningStep()
+{
+    if (current_waypoint_ < 0 || static_cast<int>(waypoints_.size()) <= current_waypoint_)
+        return;
 
-	// todo separate horz and vert velocity limits? 
-	// std::vector<double> segment_times = estimateSegmentTimes(vertices, min(dynamic_constraints_.max_vel_horz_abs, dynamic_constraints_.min_vel_horz_abs), 
-											// dynamic_constraints_.max_acceleration, magic_fabian_constant_);
-	// todo last parameter is unused in mav_trajectory_generation::estimateSegmentTimesVelocityRamp()
-  	std::vector<double> segment_times = mav_trajectory_generation::estimateSegmentTimesVelocityRamp(vertices, dynamic_constraints_.max_vel, dynamic_constraints_.max_acc, 1.0);
+    mav_trajectory_generation::timing::MiniTimer timer;
 
-  	constexpr int order_poly_const = 10; // todo 
-	mav_trajectory_generation::PolynomialOptimization<order_poly_const> opt(dimension);
-	opt.setupFromVertices(vertices, segment_times, derivative_to_optimize_);
-	mav_trajectory_generation::Trajectory* trajectory;
-	if (opt.solveLinear())
-	{
-		opt.getTrajectory(trajectory);
-	}
-	else
-	{
-		// ROS_INFO_STREAM("")
-		return false;
-	}
+    mav_msgs::EigenTrajectoryPointVector waypoints;
+    mav_msgs::EigenTrajectoryPoint current_point;
+    current_point.position_W = odometry_.position_W;
+    current_point.orientation_W_B = odometry_.orientation_W_B;
 
-	// mav_trajectory_generation::Segment::Vector segments;
-	// opt.getSegments(&segments);
+    waypoints.push_back(current_point);
+    waypoints.insert(waypoints.end(), waypoints_.begin(), waypoints_.end());
 
-	double max_vel, max_acc;
+    mav_msgs::EigenTrajectoryPointVector path;
 
-	trajectory->computeMaxVelocityAndAcceleration(&max_vel, &max_acc);
-	ROS_INFO("[pre time scaling] V max/limit: %f/%f, A max/limit: %f/%f", max_vel, dynamic_constraints_.max_vel, max_acc, dynamic_constraints_.max_acc);
+    if (planPathThroughWaypoints(waypoints, &path))
+    {
+        replacePath(path);
+        current_waypoint_ = waypoints_.size();
+    }
+    else
+    {
+        ROS_ERROR("[Mav Local Planner] Waypoint planning failed!");
+    }
 
-	trajectory->computeMaxVelocityAndAcceleration(&max_vel, &max_acc);
-	trajectory->scaleSegmentTimesToMeetConstraints(dynamic_constraints_.max_vel, dynamic_constraints_.max_acc);
-	ROS_INFO("[post time scaling] V max/limit: %f/%f, A max/limit: %f/%f", max_vel, dynamic_constraints_.max_vel, max_acc, dynamic_constraints_.max_acc);
-	return true;
+}
+
+void TrajGenHelper::waypt_vec_cb(const airsim_ros_pkgs::PointArray& waypoints_msg)
+{
+    // Plan a path from the current position to the target pose stamped.
+    ROS_INFO("[TrajGenHelper] Got a list of waypoints, %zu long!", waypoints_msg.points.size());
+    // Cancel any previous trajectory on getting a new one.
+    clear_traj();
+
+    waypoints_.clear();
+
+    for (const geometry_msgs::Pose& pose : msg.poses)
+    {
+        mav_msgs::EigenTrajectoryPoint waypoint;
+        eigenTrajectoryPointFromPoseMsg(pose, &waypoint);
+        waypoints_.push_back(waypoint);
+    }
+    current_waypoint_ = 0;
+
+    // Execute one planning step on main thread.
+    planningStep();
+    startPublishingCommands();
+}
+
+bool TrajGenHelper::fit_traj_to_waypoints(const airsim_ros_pkgs::PointArray& waypoints_msg)
+{
+    const mav_msgs::EigenTrajectoryPoint::Vector waypoints = airsim_ethz_asl_adaptor::eigenTrajectoryPointVecFromAirsimMsg(waypoints_msg);
+    const int dimension = 3; // todo param
+
+    mav_trajectory_generation::Vertex::Vector vertices;
+    int counter = 0;
+    for (auto& curr_waypt : waypoints)
+    {
+        mav_trajectory_generation::Vertex vertex(dimension);
+
+        // if first or last waypoint
+        if ((counter == 0) or (counter == waypoints.size()-1))
+        {
+            vertex.makeStartOrEnd(0, derivative_to_optimize_);
+            vertex.addConstraint(mav_trajectory_generation::derivative_order::POSITION, curr_waypt.position_W);
+        }
+        // if in the middle, add a position constraint for solver
+        // todo support higher order constraints
+        else
+        {
+            vertex.addConstraint(mav_trajectory_generation::derivative_order::POSITION, curr_waypt.position_W);  
+        }
+        vertices.push_back(vertex);
+    }
+
+    // todo separate horz and vert velocity limits? 
+    // std::vector<double> segment_times = estimateSegmentTimes(vertices, min(dynamic_constraints_.max_vel_horz_abs, dynamic_constraints_.min_vel_horz_abs), 
+                                            // dynamic_constraints_.max_acceleration, magic_fabian_constant_);
+    // todo last parameter is unused in mav_trajectory_generation::estimateSegmentTimesVelocityRamp()
+    std::vector<double> segment_times = mav_trajectory_generation::estimateSegmentTimesVelocityRamp(vertices, dynamic_constraints_.max_vel, dynamic_constraints_.max_acc, 1.0);
+
+    constexpr int order_poly_const = 10; // todo 
+    mav_trajectory_generation::PolynomialOptimization<order_poly_const> opt(dimension);
+    opt.setupFromVertices(vertices, segment_times, derivative_to_optimize_);
+    mav_trajectory_generation::Trajectory* trajectory;
+    if (opt.solveLinear())
+    {
+        opt.getTrajectory(trajectory);
+    }
+    else
+    {
+        // ROS_INFO_STREAM("")
+        return false;
+    }
+
+    // mav_trajectory_generation::Segment::Vector segments;
+    // opt.getSegments(&segments);
+
+    double max_vel, max_acc;
+
+    trajectory->computeMaxVelocityAndAcceleration(&max_vel, &max_acc);
+    ROS_INFO("[pre time scaling] V max/limit: %f/%f, A max/limit: %f/%f", max_vel, dynamic_constraints_.max_vel, max_acc, dynamic_constraints_.max_acc);
+
+    trajectory->computeMaxVelocityAndAcceleration(&max_vel, &max_acc);
+    trajectory->scaleSegmentTimesToMeetConstraints(dynamic_constraints_.max_vel, dynamic_constraints_.max_acc);
+    ROS_INFO("[post time scaling] V max/limit: %f/%f, A max/limit: %f/%f", max_vel, dynamic_constraints_.max_vel, max_acc, dynamic_constraints_.max_acc);
+    return true;
 }
