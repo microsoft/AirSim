Fixed python client QuaternionR class having bug to correctly invert the quaternion.

PythonClient/airsim/types.py
@@ -181,7 +181,7 @@ class Quaternionr(MsgpackMixin):
         return self.conjugate()
 
     def inverse(self):
-        return self.star() / self.dot(self)
+        return self.from_numpy_array(self.star().to_numpy_array() / self.dot(self))
 
     def sgn(self):
         return self/self.get_length()
@@ -191,6 +191,9 @@ class Quaternionr(MsgpackMixin):
 
     def to_numpy_array(self):
         return np.array([self.x_val, self.y_val, self.z_val, self.w_val], dtype=np.float32)
+		
+	def from_numpy_array(self, array):
+        return Quaternionr( array[0], array[1], array[2], array[3] )
 
 
 class Pose(MsgpackMixin):