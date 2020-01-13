3b1bab5a6c9186317951c1b22be5476e7a8fb17c
Chris Lovett <clovett@Microsoft.com>
2020-01-12 18:55:30 -0800

    fix build on macos platform

--- MavLinkCom/common_utils/ThreadUtils.cpp
+++ MavLinkCom/common_utils/ThreadUtils.cpp
@@ -63,10 +63,10 @@ bool CurrentThread::setThreadName(const std::string& name)
         return S_OK == (*setThreadDescriptionFunction)(GetCurrentThread(), wide_path.c_str());
     }
     return false;
+#elif defined(__APPLE__)
+    return 0 == pthread_setname_np(name.c_str());
 #else
-
-    return 0 == prctl(PR_SET_NAME, name.c_str(), 0, 0, 0);
-
+    return 0 == pthread_setname_np(pthread_self(), name.c_str());
 #endif
 
 }
