495f839e5c6f7cc454542cd98c143b292938bf81
unknown <yigalgru1>
2020-01-26 08:59:08 -0800

    IMPLEMENT A ROTATE

--- PythonClient/icd_multirotor/rotateToYaw.py
+++ PythonClient/icd_multirotor/rotateToYaw.py
@@ -2,6 +2,7 @@ import sys
 import setup_path 
 import airsim
 import pprint
+import time
 
 class RotateToYaw:
       def __init__(self, angle):
@@ -18,14 +19,18 @@ class RotateToYaw:
        #   client.rotateByYawRateAsync(float(self.angle)).join()
        #pitch, roll, throttle, yaw_rate
         #  client.moveByAngleThrottleAsync(float(self.angle),float(self.angle),float(self.angle),float(self.angle)).join()
-          vx = 1
-          vy = 0
-          duration = 5
-
+        #  vx = 1
+        #  vy = 0
+        #  duration = 5
+        #  z = -400
 # Fly given velocity vector for 5 seconds
-          speed = 1
-          delay = duration * speed
+        #  speed = 1
+        #  delay = duration * speed
+        #  client.moveToPositionAsync(-140,50, -1 * 400, 5).join()
+     #     client.moveByVelocityZAsync(vx,vy,z,duration, airsim.DrivetrainType.MaxDegreeOfFreedom, airsim.YawMode(False, 90)).join()
+      
 
+        
 
         #  state = client.getMultirotorState()
          
@@ -33,17 +38,17 @@ class RotateToYaw:
 
         #  s = pprint.pformat(state)
         #  print("state: %s" % s)
-          print("position")
-          x = client.getMultirotorState();
+        #  print("position")
+       #   x = client.getMultirotorState();
      
-          print("simSetCameraOrientation")
-          position = client.getMultirotorState().kinematics_estimated.position;
-          print(position)  
-          print(float(position.x_val))  
-          print(float(position.y_val))  
-          print(float(position.z_val))  
-          print(float(self.angle))  
-          client.moveByVelocityZAsync(vx, vy, z_val,duration, airsim.DrivetrainType.MaxDegreeOfFreedom, airsim.YawMode(False, float(self.angle))).join()
+         # print("simSetCameraOrientation")
+         # position = client.getMultirotorState().kinematics_estimated.position;
+        #  print(position)  
+        #  print(float(position.x_val))  
+         # print(float(position.y_val))  
+         # print(float(position.z_val))  
+         # print(float(self.angle))  
+        #  client.moveByVelocityZAsync(vx, vy, z_val,duration, airsim.DrivetrainType.MaxDegreeOfFreedom, airsim.YawMode(False, float(self.angle))).join()
           #client.moveToZAsync(z_val, 1).join()
           #airsim.DrivetrainType.MaxDegreeOfFreedom,airsim.YawMode(False, float(self.angle))
           #client.moveToPositionAsync(float(position.x_val),float(self.y_val), 1* float(self.z_val), 5).join() 
@@ -51,4 +56,20 @@ class RotateToYaw:
           #client.moveToPositionAsync(100*0.99,200*0.99, -330*0.99, 0.5, 10, airsim.DrivetrainType.MaxDegreeOfFreedom,airsim.YawMode(False, self.angle)).join() 
             #,airsim.YawMode(False, self.angle)).join()
           #client.rotateByYawRateAsync(30,2,"SimpleFlight").join()
+
+
+          print("Flying to 210,-320,-50")
+          client.moveToPositionAsync(210,-320,-50, 5).join() 
+
+
+          z = -50
+          duration = 1
+          speed = 1
+          delay = duration * speed
+          vx = 1
+          vy = 1
+          print("moving by velocity vx=" + str(vx) + ", vy=" + str(vy) + ", yaw=90")
+          client.moveByVelocityZAsync(vx,vy,z,duration, airsim.DrivetrainType.MaxDegreeOfFreedom, airsim.YawMode(False, float(self.angle))).join()
+          time.sleep(5)
+
           print("dam you")
