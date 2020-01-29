315353de20877b44ac5ce7f7e86a89748ebf27b0
yigalgru1 <yigalgr@matrix.co.il>
2020-01-29 03:01:07 -0800

    add websockets import

---
+++ PythonClient/webSocket/wsClientTest.py
@@ -0,0 +1,15 @@
+import asyncio
+import websockets
+
+async def hello():
+    uri = "ws://localhost:8765"
+    async with websockets.connect(uri) as websocket:
+        name = input("What's your name? ")
+
+        await websocket.send(name)
+        print(f"> {name}")
+
+        greeting = await websocket.recv()
+        print(f"< {greeting}")
+
+asyncio.get_event_loop().run_until_complete(hello())
