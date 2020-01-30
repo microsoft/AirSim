560f956d8865dbc72ccb146b131667e7ce5ad2d9
yigalgru1 <yigalgr@matrix.co.il>
2020-01-29 22:10:25 -0800

    implement a loop web sockets

---
+++ PythonClient/webSocket/wsClientLoop.py
@@ -0,0 +1,57 @@
+import asyncio
+import websockets
+import json
+import time
+
+
+async def hello():
+    uri = "ws://localhost:8765"
+    async with websockets.connect(uri) as websocket:
+        name = input("What's your name? ")
+        for i in range(0, 10):
+            telemetry = {
+                "battery_state": {
+                    "percentage": 70.04
+                },
+                "distance_from_home": 1561.4,
+                "gimbal": {
+                    "roll": 2.65,
+                    "pitch": -4.33,
+                    "yaw": -4.33
+                },
+                "height_above_takeoff": 55.77,
+                "gps_health": 5,
+                "heading": 150.00,
+                "velocity": {
+                    "x": 3.3859853744506836,
+                    "y": 4.3859853744506836,
+                    "z": -6.4859853744506836
+                },
+                "gps_position": {
+                    "latitude": 32.79549864125392,
+                    "altitude": 120.10076141357422,
+                    "longitude": 35.07356423292824
+                },
+                "last_change_time": 1577692382821,
+                "lastHome": {
+                    "latitude": 32.79549864125392,
+                    "operationalAlt": 50.1007,
+                    "longitude": 35.07356423292824
+                },
+                "owner": "droneService",
+                "state": {
+                    "armed": True
+                },
+                "wayPoints": {
+                    "status": 2
+                },
+                "keepAlive": 1577692382821
+            }
+            mesage = json.dumps(telemetry)
+            await websocket.send(mesage)
+            time.sleep(1)
+            print(f"> {name}")
+
+
+
+asyncio.get_event_loop().run_until_complete(hello())
--- PythonClient/webSocket/wsServer.py
+++ PythonClient/webSocket/wsServer.py
@@ -1,16 +1,15 @@
 import asyncio
 import websockets
 
+
 async def hello(websocket, path):
-    name = await websocket.recv()
-    print(f"< {name}")
+    while True:
+        try:
+            text = await websocket.recv()
+            print(text)
+        except websockets.ConnectionClosed:
+            break
 
-    greeting = f"Hello {name}!"
-
-    await websocket.send(greeting)
-    print(f"> {greeting}")
-
-start_server = websockets.serve(hello, "localhost", 8765)
-
+start_server = websockets.serve(hello, 'localhost', 8765)
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
