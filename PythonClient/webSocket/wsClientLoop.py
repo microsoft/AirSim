71240e4b8bc117e749702c704c34bdc0711f3266
yigalgru1 <yigalgr@matrix.co.il>
2020-01-30 04:57:53 -0800

    WebSocketClient field fill

--- PythonClient/webSocket/wsClientLoop.py
+++ PythonClient/webSocket/wsClientLoop.py
@@ -4,6 +4,7 @@ import json
 import time
 import threading
 import airsim
+import math
 
 
 class WebSocketClient:
@@ -12,25 +13,28 @@ class WebSocketClient:
     async def printit(self, websocket):
 
         while True:
-            if  WebSocketClient.isContinuousLoop:
-                rpcinfo =  self.client.getMultirotorState()
+            if WebSocketClient.isContinuousLoop:
+                rpcinfo = self.client.getMultirotorState()
                 gps_location = rpcinfo.gps_location
                 kinematics_estimated = rpcinfo.kinematics_estimated
-                rcdata = rpcinfo.rc_data
-                print(rpcinfo)
+                pitch, roll, yaw  = airsim.to_eularian_angles(rpcinfo.kinematics_estimated.orientation)
+                homepoint = self.client.getHomeGeoPoint()
+                #print(math.degrees(yaw))
+                #print(self.client.getGpsData())
+          
                 telemetry = {
                     "battery_state": {
                         "percentage": 70.04
                     },
                     "distance_from_home": 1561.4,
                     "gimbal": {
-                        "roll": rcdata.roll,
-                        "pitch": rcdata.pitch,
-                        "yaw": rcdata.yaw
+                        "roll": roll,
+                        "pitch": pitch,
+                        "yaw": yaw
                     },
                     "height_above_takeoff": 55.77,
                     "gps_health": 5,
-                    "heading": 150.00,
+                    "heading": math.degrees(yaw),
                     "velocity": {
                         "x": kinematics_estimated.linear_velocity.x_val,
                         "y": kinematics_estimated.linear_velocity.y_val,
@@ -43,9 +47,9 @@ class WebSocketClient:
                     },
                     "last_change_time": rpcinfo.timestamp,
                     "lastHome": {
-                        "latitude": 32.79549864125392,
-                        "operationalAlt": 50.1007,
-                        "longitude": 35.07356423292824
+                        "latitude": homepoint.latitude,
+                        "operationalAlt": homepoint.altitude,
+                        "longitude": homepoint.longitude
                     },
                     "owner": "droneService",
                     "state": {
@@ -59,7 +63,7 @@ class WebSocketClient:
                 mesage = json.dumps(telemetry)
                 await websocket.send(mesage)
                 await asyncio.sleep(1)
-                print(f"> {mesage}")
+                print(f"> {rpcinfo}")
 
     async def start(self):
         uri = "ws://localhost:8765"
@@ -68,6 +72,7 @@ class WebSocketClient:
         client.confirmConnection()
         client.enableApiControl(True)
         client.armDisarm(True)
+
         self.client = client
         async with websockets.connect(uri) as websocket:
             await self.printit(websocket)
