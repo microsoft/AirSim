b87297f5a1234b9fc27bdfa06e5740b22eb24460
yigalgru1 <yigalgr@matrix.co.il>
2020-02-03 22:32:03 -0800

    implement flask socket io lib

--- PythonClient/Flask_Icd/falskICD.py
+++ PythonClient/Flask_Icd/falskICD.py
@@ -3,10 +3,15 @@
 import enum
 from flask import request
 from datetime import datetime
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, Response, render_template, request, jsonify
+from flask_socketio import SocketIO, emit, send
+import airsim
+import math
+import time
 
 import json
 app = Flask(__name__)
+socketio = SocketIO(app, ping_timeout=100, ping_interval=100)
 
 posts = [{
     'author': "yigal",
@@ -18,7 +23,6 @@ posts = [{
     'title': "2",
     'content': "Second post content",
 }]
-
 
 @app.route('/')
 @app.route('/home')
@@ -166,15 +170,73 @@ def land():
 def WebSocketStart():
     if request.method == "GET":
         print("GET /WebSocket/start")
-        import sys
-        sys.path.insert(1, '../webSocket')
-        import wsClientLoop
-        from wsClientLoop import WebSocketClient
-        wbs = WebSocketClient()
-        result = wbs.start()
+        time.sleep(1)
+        air_sim = init_airsim()
+        while True:
+            data = load_airsim(air_sim)
+            print(data)            
+            # print("")
+            socketio.emit('my', data, broadcast=True)
+            time.sleep(1)
         respons = {"success": True, "message": "WebSocket start"}
         return jsonify(respons)
 
+def init_airsim():
+    airsim_client = airsim.MultirotorClient()
+    airsim_client.confirmConnection()
+    airsim_client.enableApiControl(True)
+    airsim_client.armDisarm(True)
+    return airsim_client
+
+def load_airsim(airsim_client):
+    rpcinfo = airsim_client.getMultirotorState()
+    gps_location = rpcinfo.gps_location
+    kinematics_estimated = rpcinfo.kinematics_estimated
+    pitch, roll, yaw = airsim.to_eularian_angles(
+        rpcinfo.kinematics_estimated.orientation)
+    homepoint = airsim_client.getHomeGeoPoint()
+    # print(math.degrees(yaw))
+    # print(self.client.getGpsData())
+
+    telemetry = {
+        "battery_state": {
+            "percentage": 70.04
+        },
+        "distance_from_home": 1561.4,
+        "gimbal": {
+            "roll": roll,
+            "pitch": pitch,
+            "yaw": yaw
+        },
+        "height_above_takeoff":-(kinematics_estimated.position.z_val) - kinematics_estimated.height_above_takeoff, ## -( kinematics_estimated.position.linear_velocity.z_val),
+        "gps_health": 5,
+        "heading": math.degrees(yaw),
+        "velocity": {
+            "x": kinematics_estimated.linear_velocity.x_val,
+            "y": kinematics_estimated.linear_velocity.y_val,
+            "z": kinematics_estimated.linear_velocity.z_val
+        },
+        "gps_position": {
+            "latitude": gps_location.latitude,
+            "altitude": gps_location.altitude,
+            "longitude": gps_location.longitude
+        },
+        "last_change_time": rpcinfo.timestamp,
+        "lastHome": {
+            "latitude": homepoint.latitude,
+            "operationalAlt": homepoint.altitude,
+            "longitude": homepoint.longitude
+        },
+        "owner": "droneService",
+        "state": {
+            "armed": True
+        },
+        "wayPoints": {
+            "status": 2
+        },
+        "keepAlive": rpcinfo.timestamp
+    }
+    return telemetry
 
 @app.route('/api/WebSocket/end', methods=['GET'])
 def WebSocketEnd():
@@ -189,6 +251,38 @@ def WebSocketEnd():
         respons = {"success": True, "message": "WebSocket end"}
         return jsonify(respons)
 
+
+############# Socket.io #############
+@socketio.on('connect')
+def WSocketConnect():
+    print('connect')
+
+
+@socketio.on('disconnect')
+def WSocketDisconnect():
+    print('disconnect')
+
+
+@socketio.on('keepAlive')
+def WSocketHandleKeepAlive(json):
+    # print('received keepAlive: ' + str(json))
+    pass
+
+
+@socketio.on('my')
+def handle_my_custom_event(json):
+    print('received my: ' + str(json))
+
+
+@socketio.on('force_send')
+def handle_force_send(json):
+    print('received force_send: ' + str(json))
+
+
+@socketio.on('force_stop')
+def handle_force_stop(json):
+    print('received force_stop: ' + str(json))
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
--- PythonClient/webSocket/wsClientLoop.py
+++ PythonClient/webSocket/wsClientLoop.py
@@ -13,60 +13,70 @@ class WebSocketClient:
     async def printit(self, websocket):
 
         while True:
-            if WebSocketClient.isContinuousLoop:
-                rpcinfo = self.client.getMultirotorState()
-                gps_location = rpcinfo.gps_location
-                kinematics_estimated = rpcinfo.kinematics_estimated
-                pitch, roll, yaw  = airsim.to_eularian_angles(rpcinfo.kinematics_estimated.orientation)
-                homepoint = self.client.getHomeGeoPoint()
-                #print(math.degrees(yaw))
-                #print(self.client.getGpsData())
-          
-                telemetry = {
-                    "battery_state": {
-                        "percentage": 70.04
-                    },
-                    "distance_from_home": 1561.4,
-                    "gimbal": {
-                        "roll": roll,
-                        "pitch": pitch,
-                        "yaw": yaw
-                    },
-                    "height_above_takeoff": 55.77,
-                    "gps_health": 5,
-                    "heading": math.degrees(yaw),
-                    "velocity": {
-                        "x": kinematics_estimated.linear_velocity.x_val,
-                        "y": kinematics_estimated.linear_velocity.y_val,
-                        "z": kinematics_estimated.linear_velocity.z_val
-                    },
-                    "gps_position": {
-                        "latitude": gps_location.latitude,
-                        "altitude": gps_location.altitude,
-                        "longitude": gps_location.longitude
-                    },
-                    "last_change_time": rpcinfo.timestamp,
-                    "lastHome": {
-                        "latitude": homepoint.latitude,
-                        "operationalAlt": homepoint.altitude,
-                        "longitude": homepoint.longitude
-                    },
-                    "owner": "droneService",
-                    "state": {
-                        "armed": True
-                    },
-                    "wayPoints": {
-                        "status": 2
-                    },
-                    "keepAlive": rpcinfo.timestamp
-                }
-                mesage = json.dumps(telemetry)
-                await websocket.send(mesage)
-                await asyncio.sleep(1)
-                print(f"> {rpcinfo}")
+            try:
+                if WebSocketClient.isContinuousLoop:
+                    rpcinfo = self.client.getMultirotorState()
+                    gps_location = rpcinfo.gps_location
+                    kinematics_estimated = rpcinfo.kinematics_estimated
+                    pitch, roll, yaw = airsim.to_eularian_angles(
+                        rpcinfo.kinematics_estimated.orientation)
+                    homepoint = self.client.getHomeGeoPoint()
+                    # print(math.degrees(yaw))
+                    # print(self.client.getGpsData())
+
+                    telemetry = {
+                        "battery_state": {
+                            "percentage": 70.04
+                        },
+                        "distance_from_home": 1561.4,
+                        "gimbal": {
+                            "roll": roll,
+                            "pitch": pitch,
+                            "yaw": yaw
+                        },
+                        "height_above_takeoff": 55.77,
+                        "gps_health": 5,
+                        "heading": math.degrees(yaw),
+                        "velocity": {
+                            "x": kinematics_estimated.linear_velocity.x_val,
+                            "y": kinematics_estimated.linear_velocity.y_val,
+                            "z": kinematics_estimated.linear_velocity.z_val
+                        },
+                        "gps_position": {
+                            "latitude": gps_location.latitude,
+                            "altitude": gps_location.altitude,
+                            "longitude": gps_location.longitude
+                        },
+                        "last_change_time": rpcinfo.timestamp,
+                        "lastHome": {
+                            "latitude": homepoint.latitude,
+                            "operationalAlt": homepoint.altitude,
+                            "longitude": homepoint.longitude
+                        },
+                        "owner": "droneService",
+                        "state": {
+                            "armed": True
+                        },
+                        "wayPoints": {
+                            "status": 2
+                        },
+                        "keepAlive": rpcinfo.timestamp
+                    }
+                    mesage = json.dumps(telemetry)
+                    await websocket.send(mesage)
+                    await asyncio.sleep(1)
+                    print(f"> {rpcinfo}")
+            except Exception as e:
+                   await asyncio.sleep(5)
+                   print(e)
+                   print("Reconnect")
+                   uri = "ws://10.0.0.1:7706"
+                   async with websockets.connect(uri) as websocket:
+                         await self.printit(websocket)
 
     async def start(self):
-        uri = "ws://localhost:8765"
+        uri = "ws://10.0.0.1:7700"
+        #uri = "ws://localhost:8765"
         WebSocketClient.isContinuousLoop = True
         client = airsim.MultirotorClient()
         client.confirmConnection()
