bd6ea99b45d979c11f079f7460f9537d332b68c7
yigalgru1 <yigalgr@matrix.co.il>
2020-03-03 09:53:43 +0200

    clean code

--- Unity/UnityDemo/Assets/AirSimAssets/Scripts/WebSocketManager.cs
+++ Unity/UnityDemo/Assets/AirSimAssets/Scripts/WebSocketManager.cs
@@ -27,25 +27,7 @@ namespace AirSimUnity
                 byte[] responseData = r.ReadAsByteArray();
 
             });
-
-
-            //using (var client = new HttpClient())
-            //{
-            //    client.BaseAddress = WSUri;
-            //    client.DefaultRequestHeaders.Accept.Clear();
-            //    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-
-            //    // HTTP GET
-            //    HttpResponseMessage response = await client.GetAsync(startWsCall);
-            //    if (response.IsSuccessStatusCode)
-            //    {
-            //        Console.WriteLine("Success to start WebSocket");
-            //    }
-            //    else
-            //    {
-            //        Console.WriteLine("Falied to start WebSocket");
-            //    }
-            //}
+            
         }
 
 
@@ -53,7 +35,6 @@ namespace AirSimUnity
 
         public static  void EndWS()
         {
-
             HttpClient client = new HttpClient();
             client.Get(new System.Uri("http://localhost:5000/api/WebSocket/end"), HttpCompletionOption.AllResponseContent, (r) =>
             {
@@ -68,24 +49,6 @@ namespace AirSimUnity
                 byte[] responseData = r.ReadAsByteArray();
 
             });
-
-            //using (var client = new HttpClient())
-            //{
-            //    client.BaseAddress = WSUri;
-            //    client.DefaultRequestHeaders.Accept.Clear();
-            //    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-
-            //    // HTTP GET
-            //    HttpResponseMessage response = await client.GetAsync(endWsCall);
-            //    if (response.IsSuccessStatusCode)
-            //    {
-            //        Console.WriteLine("Success to end WebSocket");
-            //    }
-            //    else
-            //    {
-            //        Console.WriteLine("Falied to end WebSocket");
-            //    }
-            //}
         }
 
     }
