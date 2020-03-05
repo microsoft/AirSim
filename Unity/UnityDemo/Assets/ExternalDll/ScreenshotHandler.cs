340d84d460d3b8d79bf6a5c58c6bf62e99833709
yigalgru1 <yigalgr@matrix.co.il>
2020-03-05 16:45:54 +0200

    add motion jpeg to airsim

---
+++ Unity/UnityDemo/Assets/ExternalDll/ScreenshotHandler.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenshotHandler : MonoBehaviour
+{
+
+
+
+    private static ScreenshotHandler instance;
+    private DLLTest.RtaVideoStreamer videoStreamer = new DLLTest.RtaVideoStreamer();
+    private Camera myCamera;
+    private bool takeScreenshotOnNextFrame;
+
+    private void Awake()
+    {
+        instance = this;
+        myCamera = GetComponent<Camera>();
+    }
+
+
+    void OnPostRender()
+    {
+        if (takeScreenshotOnNextFrame)
+        {
+            videoStreamer.StartServer(myCamera);
+            RenderTexture renderTexture = myCamera.targetTexture;
+            Texture2D renderResult = new Texture2D(renderTexture.width, renderTexture.height, TextureFormat.ARGB32, false);
+            Rect rect = new Rect(0, 0, renderTexture.width, renderTexture.height);
+            renderResult.ReadPixels(rect, 0, 0);
+            byte[] byteArray = renderResult.EncodeToPNG();
+            videoStreamer.SendMessage(byteArray);
+        }
+
+
+    }
+
+
+    public static void TakeScreenshot_Static(int width, int height)
+    {
+        instance.TakeScreenshot(width, height);
+    }
+
+    private void TakeScreenshot(int width, int height)
+    {
+        myCamera.targetTexture = RenderTexture.GetTemporary(width, height, 16);
+        takeScreenshotOnNextFrame = !takeScreenshotOnNextFrame;
+        if (!takeScreenshotOnNextFrame)
+        {
+            videoStreamer.SendMessage(null);
+            videoStreamer.StopServer();
+        }
+    }
+
+
+}
