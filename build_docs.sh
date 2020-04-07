9f4c3244c0e7f9d1bdb4d0f83328d9828dcfcbf8
Rajat Singhal <rajatsinghal564@gmail.com>
2020-04-07 12:13:52 +0530

    [docs] Add build_docs.sh, delete symlinked file

--- build_docs.sh
+++ build_docs.sh
@@ -1,6 +1,4 @@
-# make symlinks to doc folder to make mkdocs happy
-ln -s ${PWD}/CHANGELOG.md ${PWD}/docs/CHANGELOG.md
-ln -s ${PWD}/CONTRIBUTING.md ${PWD}/docs/CONTRIBUTING.md
-ln -s ${PWD}/ISSUE_TEMPLATE.md ${PWD}/docs/ISSUE_TEMPLATE.md
-ln -s ${PWD}/SUPPORT.md ${PWD}/docs/SUPPORT.md
-ln -s ${PWD}/README.md ${PWD}/docs/dREADME.md
+#!/bin/bash
+
+cp README.md docs/
+sed -i 's/](docs\//](/g' docs/README.md
