92a4145d0d60d5da5f4d916650333688b88c78ab
madratman <ratneshmadaan@gmail.com>
2020-04-06 19:19:10 -0700

    [docs] see full commit msg for details - move all remaining docs to docs/ folder - index them in mkdocs/nav - move mkdocs.yml to root folder to fix nested docs/ dir issue - add ros, mavlinkcom, mavlinkcom mocap, domain randomization, mesh vertex buffer, infrared cam, unity to gh.io page - fix srv / msg links in airsim_ros_pkgs readme

---
+++ build_docs.sh
@@ -0,0 +1,6 @@
+# make symlinks to doc folder to make mkdocs happy
+ln -s ${PWD}/CHANGELOG.md ${PWD}/docs/CHANGELOG.md
+ln -s ${PWD}/CONTRIBUTING.md ${PWD}/docs/CONTRIBUTING.md
+ln -s ${PWD}/ISSUE_TEMPLATE.md ${PWD}/docs/ISSUE_TEMPLATE.md
+ln -s ${PWD}/SUPPORT.md ${PWD}/docs/SUPPORT.md
+ln -s ${PWD}/README.md ${PWD}/docs/dREADME.md
