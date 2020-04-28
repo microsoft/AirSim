3c4545ef542575e2cef76c17b29431c83a3f9ba5
Nicholas Gyde <v-nigyde@microsoft.com>
2020-04-28 11:42:25 -0700

    Level load button, loading screen code and assets, level streaming code, options menu

---
+++ Unreal/Plugins/AirSim/Source/SimMode/LoadingScreenWidget.h
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "Runtime/UMG/Public/Components/Image.h"
+#include "LoadingScreenWidget.generated.h"
+
+
+UCLASS()
+class AIRSIM_API ULoadingScreenWidget : public UUserWidget
+{
+	GENERATED_BODY()
+//
+//public:
+//	UFUNCTION(BlueprintImplementable, "c++ interface")
+//		UImage setImage();
+//private:
+//	int count{ 0 };
+//	int num_images{ 3 };
+	
+};
