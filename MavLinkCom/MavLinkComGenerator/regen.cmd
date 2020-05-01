3fdc5a9b0cb85dc79953aed00325aab90af90e01
Chris Lovett <clovett@Microsoft.com>
2020-05-01 16:39:24 -0700

    Fix bugs affecting latest PX4 firmware version. Re-gen mavlink headers. Improve FAQ on getting VS 2019 to work with Unreal.

--- MavLinkCom/MavLinkComGenerator/regen.cmd
+++ MavLinkCom/MavLinkComGenerator/regen.cmd
@@ -1,14 +1,10 @@
 @echo off
 set SCRIPT_PATH=%~dp0
 cd %~dp0
-if "%1" == "" goto :usage
 msbuild MavLinkComGenerator.csproj
 
-bin\Debug\MavLinkComGenerator.exe -xml:%1 -out:%SCRIPT_PATH%\..\include
+bin\Debug\MavLinkComGenerator.exe -xml:%SCRIPT_PATH%\..\mavlink\message_definitions\common.xml -out:%SCRIPT_PATH%\..\include
 copy ..\include\MavLinkMessages.cpp ..\src
 del ..\include\MavLinkMessages.cpp
 
 goto :eof
-
-:usage
-echo we need the location of the mavlink 2.0 "common.xml" as input argument
