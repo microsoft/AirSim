d5724112057c8512df8366c022c8bcabc5f0f150
Rajat Singhal <rajatsinghal564@gmail.com>
2020-05-25 19:51:12 +0530

    Updated benchmark script, add save_images option

--- PythonClient/computer_vision/image_benchmarker.py
+++ PythonClient/computer_vision/image_benchmarker.py
@@ -4,22 +4,37 @@ import time
 import threading
 import numpy as np
 import cv2
+import tempfile
+import os
 
 class ImageBenchmarker():
     def __init__(self, 
             img_benchmark_type = 'simGetImages', 
-            viz_image_cv2 = False):
+            viz_image_cv2 = False,
+            save_images = False):
         self.airsim_client = airsim.VehicleClient()
         self.airsim_client.confirmConnection()
         self.image_benchmark_num_images = 0
         self.image_benchmark_total_time = 0.0
         self.image_callback_thread = None
         self.viz_image_cv2 = viz_image_cv2
+        self.save_images = save_images
+
         if img_benchmark_type == "simGetImage":
             self.image_callback_thread = threading.Thread(target=self.repeat_timer_img, args=(self.image_callback_benchmark_simGetImage, 0.05))
         if img_benchmark_type == "simGetImages":
             self.image_callback_thread = threading.Thread(target=self.repeat_timer_img, args=(self.image_callback_benchmark_simGetImages, 0.05))
         self.is_image_thread_active = False
+        self.image_callback_thread.daemon = True
+
+        if self.save_images:
+            self.tmp_dir = os.path.join(tempfile.gettempdir(), "airsim_img_bm")
+            print ("Saving images to %s" % self.tmp_dir)
+            try:
+                os.makedirs(self.tmp_dir)
+            except OSError:
+                if not os.path.isdir(self.tmp_dir):
+                    raise
 
     def start_img_benchmark_thread(self):
         if not self.is_image_thread_active:
@@ -70,9 +85,13 @@ class ImageBenchmarker():
         if self.viz_image_cv2:
             cv2.imshow("img_rgb", img_rgb)
             cv2.waitKey(1)
+        if self.save_images:
+            filename = os.path.join(self.tmp_dir, str(self.image_benchmark_num_images))
+            cv2.imwrite(os.path.normpath(filename + '.png'), img_rgb) # write to png
 
 def main(args):
-    baseline_racer = ImageBenchmarker(img_benchmark_type=args.img_benchmark_type, viz_image_cv2=args.viz_image_cv2)
+    baseline_racer = ImageBenchmarker(img_benchmark_type=args.img_benchmark_type, viz_image_cv2=args.viz_image_cv2,
+                                      save_images=args.save_images)
  
     baseline_racer.start_img_benchmark_thread()
     time.sleep(30)
@@ -83,6 +102,7 @@ if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument('--img_benchmark_type', type=str, choices=["simGetImage", "simGetImages"], default="simGetImages")
     parser.add_argument('--enable_viz_image_cv2', dest='viz_image_cv2', action='store_true', default=False)
+    parser.add_argument('--save_images', dest='save_images', action='store_true', default=False)
 
     args = parser.parse_args()
     main(args)
