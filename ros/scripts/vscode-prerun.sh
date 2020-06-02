3312111f0795aa0c7bd0becb1df8c95a975522dd
e1d1s1 <e1d1s1@hotmail.com>
2020-06-02 01:31:36 -0400

    remove unrelated script

--- ros/scripts/vscode-prerun.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# when debugging nodes with breakpoints in vscode on linux in a docker, you may need to set the envFile in launch.json for it to work in external console
-# generate the env file via prerun
-echo "exporting ROS environment"
-
-env > /tmp/ros.env
