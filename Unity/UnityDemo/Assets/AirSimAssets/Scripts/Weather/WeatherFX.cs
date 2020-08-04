ff4a94424e93a47e74c6861ddb94de3d61b719e1
Forrest Trepte <forrest@microsoft.com>
2020-08-03 18:09:09 -0700

    Unity weather parameters, weather HUD, and a visual effect for snow
    This is designed to be a Unity implementation of weather analogous to AirSim's Unreal weather system. Currently only the snow weather type is implemented.
    
    * Weather class stores global weather settings and applies them to instances of weather effects in the scene.
    * AirSimGlobal singleton provides access to global Weather object.
    * WeatherFX implements weather visuals surrounding a vehicle.
    * HUD contains weather menu for enabling weather and setting snow quantity. Triggered by F10.
    * Snow particle system.

---
+++ Unity/UnityDemo/Assets/AirSimAssets/Scripts/Weather/WeatherFX.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Renders an instance of weather visual effects.
+/// </summary>
+namespace AirSimUnity
+{
+    public class WeatherFX : MonoBehaviour
+    {
+        [SerializeField] private ParticleSystem snowParticleSystem;
+        private float snowParticleSystemMaximumRate;
+
+        private void OnValidate() {
+            Debug.Assert(snowParticleSystem != null);
+        }
+
+        private void Start() {
+            snowParticleSystemMaximumRate = snowParticleSystem.emission.rateOverTime.constant;
+        }
+
+        private void Update() {
+            Weather weather = AirSimGlobal.Instance.Weather;
+            if (!weather.IsWeatherEnabled)
+                return;
+
+            ParticleSystem.EmissionModule emission = snowParticleSystem.emission;
+            emission.rateOverTime = weather.ParamScalars[WeatherParamScalarCollection.WeatherParamScalar.Snow] * snowParticleSystemMaximumRate;
+        }
+    }
+}
