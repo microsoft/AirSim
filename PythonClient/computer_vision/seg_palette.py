e68f6ac2c7a127e3f92233775ea77ef49396fc93
Sai Vemprala <saihemachandra.v@gmail.com>
2020-09-30 17:04:35 -0700

    Segmentation palette modifications

--- PythonClient/computer_vision/seg_palette.py
+++ PythonClient/computer_vision/seg_palette.py
@@ -1,5 +1,6 @@
 import numpy
 import random
+
 # requires Python 3.5.3 :: Anaconda 4.4.0
 # pip install opencv-python
 import cv2
@@ -13,15 +14,27 @@ def generate_color_palette(numPixelsWide, outputFile):
     possibilities = [list(range(256)), list(range(256)), list(range(256))]
 
     colors = [[0] * 3 for i in range(256)]
-        
+
+    choice = 0
+    j = 0
     for i in range(3):
-        for j in range(256):
+        palette[0, j * numPixelsWide : (j + 1) * numPixelsWide, i] = choice
+        colors[j][i] = choice
+
+    for i in range(3):
+        for j in range(1, 255):
             choice = random.sample(possibilities[i], 1)[0]
             possibilities[i].remove(choice)
-            palette[0, j * numPixelsWide:(j + 1) * numPixelsWide, i] = choice
+            palette[0, j * numPixelsWide : (j + 1) * numPixelsWide, i] = choice
             colors[j][i] = choice
 
-    cv2.imwrite(outputFile, palette, [cv2.IMWRITE_PNG_COMPRESSION,0])
+    choice = 255
+    j = 255
+    for i in range(3):
+        palette[0, j * numPixelsWide : (j + 1) * numPixelsWide, i] = choice
+        colors[j][i] = choice
+
+    cv2.imwrite(outputFile, palette, [cv2.IMWRITE_PNG_COMPRESSION, 0])
 
     rgb_file = open("rgbs.txt", "w")
     for j in range(256):
@@ -29,8 +42,7 @@ def generate_color_palette(numPixelsWide, outputFile):
     rgb_file.close()
 
 
-if __name__ == '__main__':
-
+if __name__ == "__main__":
     numPixelsWide = 4
-    outputFile = 'seg_color_pallet.png'
+    outputFile = "seg_color_palette.png"
     generate_color_palette(numPixelsWide, outputFile)
