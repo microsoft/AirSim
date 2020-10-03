05a9625ca8391db50a152ad7882b08c02061f0b9
Rajat Singhal <rajatsinghal564@gmail.com>
2020-10-03 12:54:19 +0530

    [Travis] Add Ubuntu 20.04, OSX XCode 11.5 jobs (#2953)
    * [Travis] Add Ubuntu 20.04, OSX XCode 11.5 jobs
    
    * Add ROS Noetic for Ubuntu 20.04
    
    * Install Python3 libraries
    
    * Install catkin_tools from source for Ubuntu 20.04

--- tools/install_ros_deps.sh
+++ tools/install_ros_deps.sh
@@ -3,7 +3,9 @@
 set -x
 
 DISTRO="$(lsb_release -sc)"
-if [[ "$DISTRO" == "bionic" ]]; then
+if [[ "$DISTRO" == "focal" ]]; then
+    ROS_DISTRO="noetic"
+elif [[ "$DISTRO" == "bionic" ]]; then
     ROS_DISTRO="melodic"
 elif [[ "$DISTRO" == "xenial" ]]; then
     ROS_DISTRO="kinetic"
@@ -13,11 +15,12 @@ sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main
 sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 
 sudo apt update
-sudo apt install ros-$ROS_DISTRO-desktop-full
+sudo apt install -qq ros-$ROS_DISTRO-desktop-full
 
 echo "source /opt/ros/$ROS_DISTRO/setup.bash" >> ~/.bashrc
 
-sudo apt install python-rosdep python-rosinstall python-rosinstall-generator python-wstool build-essential
+sudo apt install python3-pip python3-yaml python3-setuptools
+sudo pip3 install rosdep rosinstall rospkg catkin-pkg
 sudo rosdep init
 rosdep update
 
@@ -29,6 +32,11 @@ if [[ "$DISTRO" == "xenial" ]]; then
 fi
 
 sudo apt-get install gcc-8 g++-8
-sudo apt-get install ros-$ROS_DISTRO-mavros*
-sudo apt-get install ros-$ROS_DISTRO-tf2-sensor-msgs
-sudo apt-get install python-catkin-tools
+sudo apt-get install ros-$ROS_DISTRO-mavros* ros-$ROS_DISTRO-tf2-sensor-msgs
+
+# TODO: Remove this if-block when new 0.7.0 release of catkin_tools is available
+if [[ "$DISTRO" == "focal" ]]; then
+    sudo pip3 install "git+https://github.com/catkin/catkin_tools.git#egg=catkin_tools"
+else
+    sudo pip3 install catkin-tools
+fi
