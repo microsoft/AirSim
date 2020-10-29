511e8472982ed4ac84b498a4df7ccf86c4876513
Niek van Leeuwen <niekvleeuwen@hotmail.nl>
2020-10-29 12:19:20 +0100

    Added support for python client

--- PythonClient/airsim/client.py
+++ PythonClient/airsim/client.py
@@ -110,6 +110,15 @@ class VehicleClient:
             seconds (float): Time to run the simulation for
         """
         self.client.call('simContinueForTime', seconds)
+    
+    def simContinueForFrames(self, frames):
+        """
+        Continue the simulation for the specified number of frames
+
+        Args:
+            frames (float): Frames to run the simulation for
+        """
+        self.client.call('simContinueForFrames', frames)
 
     def getHomeGeoPoint(self, vehicle_name = ''):
         """
