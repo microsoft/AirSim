39b278170051ea9eb1cc66e78f19b21b0143b427
Ahmed Elsaharti <ahmed.elsaharti@und.edu>
2021-02-04 12:06:46 -0600

    Fixed bad symlink dir

--- PX4Scripts/run_airsim_sitl.sh
+++ PX4Scripts/run_airsim_sitl.sh
@@ -6,9 +6,8 @@ PARENT_DIR="$(dirname "$SCRIPT_DIR")"
 BUILD_DIR=$PARENT_DIR/Firmware/ROMFS/px4fmu_common
 instance_path=$PARENT_DIR/Firmware/build/px4_sitl_default
 BIN_DIR=$PARENT_DIR/Firmware/build/px4_sitl_default/bin/px4
-STARTUP_DIR=$PARENT_DIR/Firmware/etc/init.d-posix/rcS
 TEST_DATA=$PARENT_DIR/Firmware/test_data
 working_dir="$instance_path/instance_$instance_num"
 [ ! -d "$working_dir" ] && mkdir -p "$working_dir"
 pushd "$working_dir" &>/dev/null
-$BIN_DIR -i $instance_num $BUILD_DIR -s $STARTUP_DIR -t $TEST_DATA
+$BIN_DIR -i $instance_num $BUILD_DIR -s "etc/init.d-posix/rcS" -t $TEST_DATA
