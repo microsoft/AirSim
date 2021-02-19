da0bcf09d81f26e93f3bb54ec8387a34e8c84fb5
Ahmed Elsaharti <ahmed.elsaharti@und.edu>
2021-02-19 15:19:48 -0600

    Update MultiRotorParams.hpp

--- AirLib/include/vehicles/multirotor/MultiRotorParams.hpp
+++ AirLib/include/vehicles/multirotor/MultiRotorParams.hpp
@@ -223,9 +223,10 @@ protected: //static utility functions for derived classes to use
         std::vector<real_T> arm_lengths(params.rotor_count, 0.2275f);
 
         //set up mass
-        //this has to be between max_thrust*rotor_count/10 (1.6kg using default parameters in RotorParams.hpp) and 0.5*max_thrust*rotor_count/10 (0.8kg using default parameters)
+        //this has to be between max_thrust*rotor_count/10 (1.6kg using default parameters in RotorParams.hpp) and (idle throttle percentage)*max_thrust*rotor_count/10 (0.8kg using default parameters and SimpleFlight)
         //any value above the maximum would result in the motors not being able to lift the body even at max thrust,
-        //and any value below the minimum would cause the drone to fly upwards on idling throttle (50% of the max throttle)
+        //and any value below the minimum would cause the drone to fly upwards on idling throttle (50% of the max throttle if using SimpleFlight)
+        //Note that the default idle throttle percentage is 50% if you are using SimpleFlight
         params.mass = 1.0f; 
 
         real_T motor_assembly_weight = 0.055f;  //weight for MT2212 motor for F450 frame
@@ -254,9 +255,9 @@ protected: //static utility functions for derived classes to use
         std::vector<real_T> arm_lengths(params.rotor_count, 0.2275f);
 
         //set up mass
-        //this has to be between max_thrust*rotor_count/10 (2.5kg using default parameters in RotorParams.hpp) and 0.5*max_thrust*rotor_count/10 (1.25kg using default parameters)
+        //this has to be between max_thrust*rotor_count/10 (2.5kg using default parameters in RotorParams.hpp) and (idle throttle percentage)*max_thrust*rotor_count/10 (1.25kg using default parameters and SimpleFlight)
         //any value above the maximum would result in the motors not being able to lift the body even at max thrust,
-        //and any value below the minimum would cause the drone to fly upwards on idling throttle (50% of the max throttle)
+        //and any value below the minimum would cause the drone to fly upwards on idling throttle (50% of the max throttle if using SimpleFlight)
         params.mass = 1.0f;
 
         real_T motor_assembly_weight = 0.055f;  //weight for MT2212 motor for F450 frame
