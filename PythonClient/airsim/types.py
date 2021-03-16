85b2fa35945b757b476ce07cb460af76a808ff12
Sai Vemprala <saihemachandra.v@gmail.com>
2021-03-16 00:50:04 -0700

    Add metaclass over ImageType to raise error upon wrong usage

--- PythonClient/airsim/types.py
+++ PythonClient/airsim/types.py
@@ -30,6 +30,33 @@ class ImageType:
     SurfaceNormals = 6
     Infrared = 7
 
+class _ImageType(type):
+    @property
+    def Scene(cls):
+        return 0
+    def DepthPlanar(cls):
+        return 1
+    def DepthPerspective(cls):
+        return 2
+    def DepthVis(cls):
+        return 3
+    def DisparityNormalized(cls):
+        return 4
+    def Segmentation(cls):
+        return 5
+    def SurfaceNormals(cls):
+        return 6
+    def Infrared(cls):
+        return 7
+
+    def __getattr__(self, key):
+        if key == 'DepthPlanner':
+            print('\033[31m'+"DepthPlanner has been (correctly) renamed to DepthPlanar. Please use ImageType.DepthPlanar instead."+'\033[0m')
+            raise AttributeError
+
+class ImageType(metaclass=_ImageType):
+    pass
+
 class DrivetrainType:
     MaxDegreeOfFreedom = 0
     ForwardOnly = 1
