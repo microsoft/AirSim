246ac30c98b1f0edc361e80817d69f2a3440c1a5
Chris Lovett <clovett@microsoft.com>
2021-04-14 19:32:39 -0700

    revert change

--- MavLinkCom/src/serial_com/wifi.cpp
+++ MavLinkCom/src/serial_com/wifi.cpp
@@ -9,7 +9,7 @@
 #include <linux/wireless.h>
 #include <stdlib.h>
 #include <string.h>
-#include "mavlink_utils/Utils.hpp"
+#include "common_utils/Utils.hpp"
 
 int getWifiRssi(int socket, const char* ifaceName)
 {
