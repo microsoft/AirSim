30ab123d36818e3402e66504228865aba5e8bf66
Chris Lovett <clovett@microsoft.com>
2021-04-16 15:56:18 -0700

    fix unreal build

--- Unreal/Plugins/AirSim/Source/UnrealSensors/UnrealSensorFactory.cpp
+++ Unreal/Plugins/AirSim/Source/UnrealSensors/UnrealSensorFactory.cpp
@@ -9,17 +9,17 @@ UnrealSensorFactory::UnrealSensorFactory(AActor* actor, const NedTransform* ned_
     setActor(actor, ned_transform);
 }
 
-std::unique_ptr<msr::airlib::SensorBase> UnrealSensorFactory::createSensorFromSettings(
+std::shared_ptr<msr::airlib::SensorBase> UnrealSensorFactory::createSensorFromSettings(
     const AirSimSettings::SensorSetting* sensor_setting) const
 {
     using SensorBase = msr::airlib::SensorBase;
 
     switch (sensor_setting->sensor_type) {
     case SensorBase::SensorType::Distance:
-        return std::unique_ptr<UnrealDistanceSensor>(new UnrealDistanceSensor(
+        return std::shared_ptr<UnrealDistanceSensor>(new UnrealDistanceSensor(
             *static_cast<const AirSimSettings::DistanceSetting*>(sensor_setting), actor_, ned_transform_));
     case SensorBase::SensorType::Lidar:
-        return std::unique_ptr<UnrealLidarSensor>(new UnrealLidarSensor(
+        return std::shared_ptr<UnrealLidarSensor>(new UnrealLidarSensor(
             *static_cast<const AirSimSettings::LidarSetting*>(sensor_setting), actor_, ned_transform_));
     default:
         return msr::airlib::SensorFactory::createSensorFromSettings(sensor_setting);
