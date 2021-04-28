e894d3a21521d4df2a247f8bae20e108147a3493
Chris Lovett <clovett@microsoft.com>
2021-04-27 18:35:30 -0700

    script to test flying up high

---
+++ PythonClient/multirotor/high.py
@@ -0,0 +1,52 @@
+import setup_path
+import airsim
+
+import sys
+import time
+
+print("""This script is designed to fly on the streets of the Neighborhood environment
+and assumes the unreal position of the drone is [160, -1500, 120].""")
+
+client = airsim.MultirotorClient()
+client.confirmConnection()
+client.enableApiControl(True)
+
+print("arming the drone...")
+client.armDisarm(True)
+
+state = client.getMultirotorState()
+if state.landed_state == airsim.LandedState.Landed:
+    print("taking off...")
+    client.takeoffAsync().join()
+else:
+    client.hoverAsync().join()
+
+time.sleep(1)
+
+state = client.getMultirotorState()
+if state.landed_state == airsim.LandedState.Landed:
+    print("take off failed...")
+    sys.exit(1)
+
+# AirSim uses NED coordinates so negative axis is up.
+# z of -50 is 50 meters above the original launch point.
+z = -50
+print("make sure we are hovering at 50 meters...")
+client.moveToZAsync(z, 1).join()
+
+# see https://github.com/Microsoft/AirSim/wiki/moveOnPath-demo
+
+# this method is async and we are not waiting for the result since we are passing timeout_sec=0.
+
+time.sleep(1)
+
+# come down quickly
+z = -7
+client.moveToZAsync(z, 5).join()
+
+print("landing...")
+client.landAsync().join()
+print("disarming...")
+client.armDisarm(False)
+client.enableApiControl(False)
+print("done.")
