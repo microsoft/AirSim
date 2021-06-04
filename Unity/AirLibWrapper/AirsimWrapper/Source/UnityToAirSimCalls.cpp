3979e6a9faac1cd904151041c36bc8c887374dea
Yvo Keuter <ypmkeuter@gmail.com>
2021-06-04 20:26:59 +0200

    apply style from clang-format

--- Unity/AirLibWrapper/AirsimWrapper/Source/UnityToAirSimCalls.cpp
+++ Unity/AirLibWrapper/AirsimWrapper/Source/UnityToAirSimCalls.cpp
@@ -2,6 +2,6 @@
 
 void StartServerThread(std::string sim_mode_name, int port_number)
 {
-	key = new SimHUD(sim_mode_name, port_number);
-	key->BeginPlay();
+    key = new SimHUD(sim_mode_name, port_number);
+    key->BeginPlay();
 }
--- Unity/AirLibWrapper/AirsimWrapper/Source/UnityToAirSimCalls.h
+++ Unity/AirLibWrapper/AirsimWrapper/Source/UnityToAirSimCalls.h
@@ -6,9 +6,9 @@
 #include "Logger.h"
 
 #ifdef _WIN32
-	#define EXPORT __declspec(dllexport)
+#define EXPORT __declspec(dllexport)
 #else
-	#define EXPORT __attribute__((visibility("default")))
+#define EXPORT __attribute__((visibility("default")))
 #endif
 
 static SimHUD* key = nullptr;
@@ -17,44 +17,40 @@ void StartServerThread(std::string sim_mode_name, int port_number);
 
 extern "C" EXPORT bool StartServer(char* sim_mode_name, int port_number)
 {
-	LOGGER->WriteLog("Starting server for : " + std::string(sim_mode_name));
-	std::thread server_thread(StartServerThread, sim_mode_name, port_number);
-	server_thread.detach();
-	int waitCounter = 25; // waiting for maximum 5 seconds to start a server.
-	while ((key == nullptr || !key->server_started_Successfully_) && waitCounter > 0)
-	{
-		std::this_thread::sleep_for(std::chrono::milliseconds(200));
-		waitCounter--;
-	}
-	return key->server_started_Successfully_;
+    LOGGER->WriteLog("Starting server for : " + std::string(sim_mode_name));
+    std::thread server_thread(StartServerThread, sim_mode_name, port_number);
+    server_thread.detach();
+    int waitCounter = 25; // waiting for maximum 5 seconds to start a server.
+    while ((key == nullptr || !key->server_started_Successfully_) && waitCounter > 0) {
+        std::this_thread::sleep_for(std::chrono::milliseconds(200));
+        waitCounter--;
+    }
+    return key->server_started_Successfully_;
 }
 
 extern "C" EXPORT void StopServer()
 {
-	key->EndPlay();
-	if (key != nullptr)
-	{
-		delete key;
-		key = nullptr;
-	}
-		LOGGER->WriteLog("Server stopped");
+    key->EndPlay();
+    if (key != nullptr) {
+        delete key;
+        key = nullptr;
+    }
+    LOGGER->WriteLog("Server stopped");
 }
 
 extern "C" EXPORT void CallTick(float deltaSeconds)
 {
-	key->Tick(deltaSeconds);
+    key->Tick(deltaSeconds);
 }
 
 extern "C" EXPORT void InvokeCollisionDetection(char* vehicle_name, AirSimUnity::AirSimCollisionInfo collision_info)
 {
-	auto simMode = key->GetSimMode();
-	if (simMode)
-	{
-		auto vehicleApi = simMode->getVehicleSimApi(vehicle_name);
-		if (vehicleApi)
-		{
-			msr::airlib::CollisionInfo collisionInfo = UnityUtilities::Convert_to_AirSimCollisioinInfo(collision_info);
-			vehicleApi->OnCollision(collisionInfo);
-		}
-	}
+    auto simMode = key->GetSimMode();
+    if (simMode) {
+        auto vehicleApi = simMode->getVehicleSimApi(vehicle_name);
+        if (vehicleApi) {
+            msr::airlib::CollisionInfo collisionInfo = UnityUtilities::Convert_to_AirSimCollisioinInfo(collision_info);
+            vehicleApi->OnCollision(collisionInfo);
+        }
+    }
 }
