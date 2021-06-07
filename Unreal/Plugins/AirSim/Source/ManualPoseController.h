f33bce58a7831f47c224e399e8b0cba39b7318ca
zimmy87 <zimmy87@gmail.com>
2021-06-07 07:35:27 -0700

    apply style from clang-format

--- Unreal/Plugins/AirSim/Source/ManualPoseController.h
+++ Unreal/Plugins/AirSim/Source/ManualPoseController.h
@@ -7,7 +7,8 @@
 #include "ManualPoseController.generated.h"
 
 UCLASS()
-class AIRSIM_API UManualPoseController : public UObject {
+class AIRSIM_API UManualPoseController : public UObject
+{
     GENERATED_BODY()
 
 public:
@@ -35,15 +36,15 @@ private:
     void inputManualSpeedIncrease(float val);
     void inputManualSpeedDecrease(float val);
 
-    void setupInputBindings();	
+    void setupInputBindings();
     void removeInputBindings();
     void clearBindings();
 
 private:
     FInputAxisBinding *left_binding_, *right_binding_, *up_binding_, *down_binding_;
     FInputAxisBinding *forward_binding_, *backward_binding_, *left_yaw_binding_, *right_yaw_binding_;
-    FInputAxisBinding *up_pitch_binding_, *down_pitch_binding_,  *left_roll_binding_, *right_roll_binding_;
-    FInputAxisBinding* inc_speed_binding_, *dec_speed_binding_;
+    FInputAxisBinding *up_pitch_binding_, *down_pitch_binding_, *left_roll_binding_, *right_roll_binding_;
+    FInputAxisBinding *inc_speed_binding_, *dec_speed_binding_;
 
     FInputAxisKeyMapping left_mapping_, right_mapping_, up_mapping_, down_mapping_;
     FInputAxisKeyMapping forward_mapping_, backward_mapping_, left_yaw_mapping_, right_yaw_mapping_;
@@ -53,7 +54,7 @@ private:
     FVector delta_position_;
     FRotator delta_rotation_;
 
-    AActor *actor_;
+    AActor* actor_;
 
     float acceleration_ = 0, speed_scaler_ = 1000;
     FVector input_positive_, inpute_negative_;
