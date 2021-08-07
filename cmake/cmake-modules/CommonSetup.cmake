efea40620e2bd42934fe162b8b967abffe92c17c
Rajat Singhal <rajatsinghal564@gmail.com>
2021-08-07 12:27:59 +0530

    CMake: Remove -Wno-parentheses flag, cleanup

--- cmake/cmake-modules/CommonSetup.cmake
+++ cmake/cmake-modules/CommonSetup.cmake
@@ -44,13 +44,14 @@ macro(CommonSetup)
     IF(UNIX)
         set(RPC_LIB_DEFINES "-D MSGPACK_PP_VARIADICS_MSVC=0")
         set(BUILD_TYPE "linux")
+        set(CMAKE_CXX_STANDARD 17)
+
         if (APPLE)
-            set(CMAKE_CXX_STANDARD 17)
             set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Wextra -Wstrict-aliasing -D__CLANG__")
         else ()
             set(CMAKE_CXX_FLAGS "\
-                -std=c++17 -ggdb -Wall -Wextra \
-                -Wno-variadic-macros -Wno-parentheses -Wno-unused-function -Wno-unused \
+                -ggdb -Wall -Wextra \
+                -Wno-variadic-macros -Wno-unused-function -Wno-unused \
                 -pthread \
                 ${RPC_LIB_DEFINES} ${CMAKE_CXX_FLAGS}")
 
