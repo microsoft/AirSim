0ac054924bee2bb0c963f37cecfe44e6c01290d3
Paul Kozin <kozinp@yandex.ru>
2021-08-18 23:14:53 +0300

    fixed ros build

--- cmake/3rdparty/rpclib/variables.cmake
+++ cmake/3rdparty/rpclib/variables.cmake
@@ -1,3 +1,7 @@
 set(3RDPARTY_OPTIONS \"${CMAKE_ARGS}\")
+if (UNIX)
+    list(APPEND 3RDPARTY_OPTIONS "-DCMAKE_POSITION_INDEPENDENT_CODE=ON")
+endif()
+
 set(GIT_REPOSITORY https://github.com/rpclib/rpclib)
 set(GIT_TAG v2.3.0)
