f4fdcefb1aca40b1f4c457dddc5429a84ea32744
alonfaraj <alonfaraj@gmail.com>
2021-08-18 08:52:34 -0700

    - add launch files

---
+++ ros2/src/airsim_ros_pkgs/launch/static_transforms.launch.py
@@ -0,0 +1,21 @@
+import os
+import sys
+
+import launch
+import launch_ros.actions
+
+
+def generate_launch_description():
+    ld = launch.LaunchDescription([
+        launch_ros.actions.Node(
+            package='tf2_ros',
+            executable='static_transform_publisher',
+            name='ned_to_enu_pub',
+            arguments=['0', '0', '0', '1.57', '0', '3.14', 'world_ned', 'world_enu']#, '100']
+        )
+    ])
+    return ld
+
+
+if __name__ == '__main__':
+    generate_launch_description()
