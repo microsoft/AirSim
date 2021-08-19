2f49e359ff936231c9a4dab20ae5201225d50f6d
alonfaraj <alonfaraj@gmail.com>
2021-08-19 05:56:24 -0700

    - change airsim_node launch file

--- ros2/src/airsim_ros_pkgs/launch/airsim_node.launch.py
+++ ros2/src/airsim_ros_pkgs/launch/airsim_node.launch.py
@@ -1,65 +1,97 @@
 import os
 import sys
 
-import launch
-import launch_ros.actions
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
 from ament_index_python.packages import get_package_share_directory
 
 
 def generate_launch_description():
-    ld = launch.LaunchDescription([
-        launch.actions.DeclareLaunchArgument(
-            name='output',
-            default_value='log'
-        ),
-        launch.actions.DeclareLaunchArgument(
-            name='publish_clock',
-            default_value='false'
-        ),
-        launch.actions.DeclareLaunchArgument(
-            name='is_vulkan',
-            default_value='true'
-        ),
-        launch.actions.DeclareLaunchArgument(
-            name='host',
-            default_value='192.168.211.1' #ToDo - cahnge
-        ),
-        launch_ros.actions.Node(
+
+    output = DeclareLaunchArgument(
+        "output",
+        default_value='log')
+
+    publish_clock = DeclareLaunchArgument(
+        "publish_clock",
+        default_value='False')
+
+    is_vulkan = DeclareLaunchArgument(
+        "is_vulkan",
+        default_value='True')
+
+    host = DeclareLaunchArgument(
+        "host",
+        default_value='localhost')
+  
+    airsim_node = Node(
             package='airsim_ros_pkgs',
             executable='airsim_node',
             name='airsim_node',
-            output='screen', #ToDo-change to var
-            parameters=[
-                {
-                    'is_vulkan': 'false'
-                },
-                {
-                    'update_airsim_img_response_every_n_sec': '0.05'
-                },
-                {
-                    'update_airsim_control_every_n_sec': '0.01'
-                },
-                {
-                    'update_lidar_every_n_sec': '0.01'
-                },
-                {
-                    'publish_clock': 'false' #launch.substitutions.LaunchConfiguration('publish_clock') #ToDo-change to var
-                },
-                {
-                    'host_ip': '192.168.211.1'#launch.substitutions.LaunchConfiguration('host') #ToDo-change to var
-                }
-            ],
-            arguments=['--ros-args', '--log-level', 'INFO']
-        )#,
+            output='screen',
+            parameters=[{
+                'is_vulkan': False,
+                'update_airsim_img_response_every_n_sec': 0.05,
+                'update_airsim_control_every_n_sec': 0.01,
+                'update_lidar_every_n_sec': 0.01,
+                'publish_clock': LaunchConfiguration('publish_clock'),
+                'host_ip': LaunchConfiguration('host')
+            }])
+
+
+    # Create the launch description and populate
+    ld = LaunchDescription()
+
+    # Declare the launch options
+    ld.add_action(output)
+    ld.add_action(publish_clock)
+    ld.add_action(is_vulkan)
+    ld.add_action(host)
+  
+    ld.add_action(airsim_node)
+
+
+
+
+
+        # launch.actions.DeclareLaunchArgument(
+
+        # launch_ros.actions.Node(
+        #     package='airsim_ros_pkgs',
+        #     executable='airsim_node',
+        #     name='airsim_node',
+        #     output='screen', #ToDo-change to var
+        #     parameters=[
+        #         {
+        #             'is_vulkan': 'false'
+        #         },
+        #         {
+        #             'update_airsim_img_response_every_n_sec': '0.05'
+        #         },
+        #         {
+        #             'update_airsim_control_every_n_sec': '0.01'
+        #         },
+        #         {
+        #             'update_lidar_every_n_sec': '0.01'
+        #         },
+        #         {
+        #             'publish_clock': 'false' #launch.substitutions.LaunchConfiguration('publish_clock') #ToDo-change to var
+        #         },
+        #         {
+        #             'host_ip': launch.substitutions.LaunchConfiguration('host') #ToDo-change to var
+        #         }
+        #     ],
+        #     arguments=['--ros-args', '--log-level', 'INFO']
+        # )#,
         # launch.actions.IncludeLaunchDescription(
         #     launch.launch_description_sources.PythonLaunchDescriptionSource(
         #         os.path.join(get_package_share_directory(
         #             'airsim_ros_pkgs'), 'launch/static_transforms.launch.py')
         #     )
         # )
-    ])
+ #   ])
+
     return ld
-
-
-if __name__ == '__main__':
-    generate_launch_description()
