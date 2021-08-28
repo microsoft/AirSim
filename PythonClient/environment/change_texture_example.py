a397215775c576eb729102e702635d05c738b891
Sai Vemprala <saihemachandra.v@gmail.com>
2021-08-27 23:13:41 -0700

    Add sample texture image

--- PythonClient/environment/change_texture_example.py
+++ PythonClient/environment/change_texture_example.py
@@ -1,7 +1,7 @@
 import airsim
-import time
 
 c = airsim.MultirotorClient()
 c.confirmConnection()
 
-c.simSetObjectMaterialFromTexture('OrangeBall', 'C:\\Users\\savempra\\Desktop\\jupi.jpg')
+c.simSetObjectMaterialFromTexture("OrangeBall", "sample_texture.jpg")
+
