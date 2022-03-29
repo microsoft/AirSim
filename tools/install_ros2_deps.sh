0f80896b33dd0365adc0f89ffd39ca5248b101e9
alonfaraj <alonfaraj@gmail.com>
2022-03-29 16:17:44 +0300

    fix install ros-base

--- tools/install_ros2_deps.sh
+++ tools/install_ros2_deps.sh
@@ -19,7 +19,7 @@ sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(source /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
 
 sudo apt-get update
-sudo apt-get install -y ros-ROS_DISTRO-ros-base
+sudo apt-get install -y ros-$ROS_DISTRO-ros-base
 
 echo "source /opt/ros/$ROS_DISTRO/setup.bash" >> ~/.bashrc
 
