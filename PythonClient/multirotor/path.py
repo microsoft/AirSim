aa9bba76cdf67f501efae01271e8df6ec38bea49
tnilest <tnilest@uky.edu>
2022-05-18 13:46:27 -0400

    Edit path.py to move drone in HallwayExample

--- PythonClient/multirotor/path.py
+++ PythonClient/multirotor/path.py
@@ -30,7 +30,7 @@ if state.landed_state == airsim.LandedState.Landed:
 
 # AirSim uses NED coordinates so negative axis is up.
 # z of -5 is 5 meters above the original launch point.
-z = -5
+z = -3
 print("make sure we are hovering at {} meters...".format(-z))
 client.moveToZAsync(z, 1).join()
 
@@ -39,11 +39,11 @@ client.moveToZAsync(z, 1).join()
 # this method is async and we are not waiting for the result since we are passing timeout_sec=0.
 
 print("flying on path...")
-result = client.moveOnPathAsync([airsim.Vector3r(125,0,z),
-                                airsim.Vector3r(125,-130,z),
-                                airsim.Vector3r(0,-130,z),
+result = client.moveOnPathAsync([airsim.Vector3r(0,170,z),
+                                airsim.Vector3r(50,170,z),
+                                airsim.Vector3r(0,170,z),
                                 airsim.Vector3r(0,0,z)],
-                        12, 120,
+                        5, 120,
                         airsim.DrivetrainType.ForwardOnly, airsim.YawMode(False,0), 20, 1).join()
 
 # drone will over-shoot so we bring it back to the start point before landing.
