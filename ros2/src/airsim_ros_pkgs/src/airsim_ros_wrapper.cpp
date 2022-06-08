b687a4af42efe93204caad54ed0d6921e7c9f620
David Tejero Ruiz <dtejero@catec.aero>
2022-06-08 15:06:54 +0200

    Added 'ring' field in lidar point cloud generation. Updated ROS and ROS2 lidar data processing

--- ros2/src/airsim_ros_pkgs/src/airsim_ros_wrapper.cpp
+++ ros2/src/airsim_ros_pkgs/src/airsim_ros_wrapper.cpp
@@ -651,14 +651,15 @@ sensor_msgs::msg::PointCloud2 AirsimROSWrapper::get_lidar_msg_from_airsim(const 
     lidar_msg.header.stamp = rclcpp::Time(lidar_data.time_stamp);
     lidar_msg.header.frame_id = vehicle_name + "/" + sensor_name;
 
-    if (lidar_data.point_cloud.size() > 3) {
+    if (lidar_data.point_cloud.size() > 4) {
         lidar_msg.height = 1;
-        lidar_msg.width = lidar_data.point_cloud.size() / 3;
+        lidar_msg.width = lidar_data.point_cloud.size() / 4;
 
-        lidar_msg.fields.resize(3);
+        lidar_msg.fields.resize(4);
         lidar_msg.fields[0].name = "x";
         lidar_msg.fields[1].name = "y";
         lidar_msg.fields[2].name = "z";
+        lidar_msg.fields[3].name = "ring";
 
         int offset = 0;
 
