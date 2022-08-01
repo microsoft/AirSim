ecdcdb811f25f2ce0f20bc1aeb284773c48e7848
Nikola Jovicic <jovicicnikola@outlook.com>
2022-08-01 15:36:30 +0200

    Reformat file

--- ros2/src/airsim_ros_pkgs/src/airsim_ros_wrapper.cpp
+++ ros2/src/airsim_ros_pkgs/src/airsim_ros_wrapper.cpp
@@ -1038,8 +1038,6 @@ rclcpp::Time AirsimROSWrapper::update_state()
     return curr_ros_time;
 }
 
-
-
 void AirsimROSWrapper::publish_vehicle_state()
 {
     for (auto& vehicle_name_ptr_pair : vehicle_name_ptr_map_) {
