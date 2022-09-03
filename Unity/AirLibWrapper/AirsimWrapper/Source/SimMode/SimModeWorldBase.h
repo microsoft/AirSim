e32219f6cff2ce580a3a69e825fad4e1f40d3c8f
Tyler <tyler.fedrizzi@gmail.com>
2022-09-03 06:03:11 -0400

    Fix namespacing of vector3r

--- Unity/AirLibWrapper/AirsimWrapper/Source/SimMode/SimModeWorldBase.h
+++ Unity/AirLibWrapper/AirsimWrapper/Source/SimMode/SimModeWorldBase.h
@@ -37,7 +37,7 @@ public:
     virtual void pause(bool is_paused) override;
     virtual void continueForTime(double seconds) override;
     virtual void setWind(const msr::airlib::Vector3r& wind) const override;
-    virtual void setExtForce(const Vector3r& ext_force) const override;
+    virtual void setExtForce(const msr::airlib::Vector3r& ext_force) const override;
 
 private:
     std::unique_ptr<msr::airlib::PhysicsWorld> physics_world_;
