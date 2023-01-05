695d64a8538646f83841c9fc8967392c93dc884b
Branton Davis <branton.davis@dronebase.com>
2023-01-05 17:18:29 -0600

    clean up

--- ros2/README.md
+++ ros2/README.md
@@ -1,7 +1,7 @@
 # AirSim ROS 2 Node
 
 This fork of Microsoft's AirSim project contains a few changes to the ROS 2 node:
-- Custom Dockerfile and build/run script.
+- Dockerfile to run the node in docker and associated build/run script.
 - Fixes bug with some image types being published that are incompatible with rqt.
 - Adds gimbal state topics publishing the vehicle relative orientation of the gimbal.
 - Uses camera settings when updating the gimbal orientation (on subscription) to maintain the correct position according to the settings JSON.
--- ros2/run.sh
+++ ros2/run.sh
@@ -61,27 +61,6 @@ function source_getopts_long() {
     source "${SCRIPT_DIR}/getopts_long/lib/getopts_long.bash"
 }
 
-function docker_x() {
-    CONTAINER_NAME="${1}"
-    IMAGE_NAME="${2}"
-    DOCKER_ARGS=${3}
-    ENTRYPOINT_ARGS=${4}
-
-    docker run --rm -it \
-        --gpus='all,"capabilities=compute,utility,graphics,display"' \
-        --env="DISPLAY=$DISPLAY" \
-        --env="QT_X11_NO_MITSHM=1" \
-        --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
-        --env="XAUTHORITY=$XAUTH" \
-        --volume="$XAUTH:$XAUTH" \
-        --net=host \
-        --name="${CONTAINER_NAME}" \
-        --privileged \
-        --env="SDL_HINT_CUDA_DEVICE='0'" \
-        --volume="$(pwd)/airsim/RECORDING:/RECORDING" \
-        ${DOCKER_ARGS} ${ENTRYPOINT_OVERRIDE} ${IMAGE_NAME} ${ENTRYPOINT_ARGS}
-}
-
 function run_airsim() {
     ensure_build_airsim
 
