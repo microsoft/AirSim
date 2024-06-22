ec0d04383c47b925c21fea4a1734cb1f963e981d
Tyler Fedrizzi <tyler.fedrizzi@gmail.com>
2024-06-22 18:23:31 -0400

    Upgrade the CMake version to 12

--- setup.sh
+++ setup.sh
@@ -60,7 +60,7 @@ else #linux
         wget -O - http://apt.llvm.org/llvm-snapshot.gpg.key | sudo apt-key add -
         sudo apt-get update
     fi
-    sudo apt-get install -y clang-8 clang++-8 libc++-8-dev libc++abi-8-dev
+    sudo apt-get install -y clang-12 clang++-12 libc++-12-dev libc++abi-12-dev
 fi
 
 if ! which cmake; then
